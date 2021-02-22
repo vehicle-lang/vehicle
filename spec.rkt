b1f23f4d9bb909bd21cf1cb1726d4b7e8444338c
Wen Kokke <wenkokke@users.noreply.github.com>
2021-02-22 11:44:02 +0000

    Syntax spec.

---
+++ spec.rkt
@@ -0,0 +1,86 @@
+#lang racket
+(require redex)
+
+;; Syntax
+(define-language
+  vehicle
+
+  ;; Terms
+  (e ::=
+     ;; Core calculus
+     x
+     (λ [x τ] e)
+     (e_1 e_2)
+     (Λ [α κ] e)
+     (e [ τ ])
+     ;; Numbers
+     number
+     (tensor e ...)
+     ;; Arithmetic (numbers and tensors)
+     (- e)
+     (+ e_1 e_2)
+     (- e_1 e_2)
+     (* e_1 e_2)
+     (/ e_1 e_2)
+     (abs e)
+     (min e)
+     (max e)
+     (dot e_1 e_2)
+     ;; Conditionals
+     (if e_1 e_2 e_3)
+     (== e_1 e_2)
+     (<= e_1 e_2)
+     (>= e_1 e_2)
+     (< e_1 e_2)
+     (> e_1 e_2)
+     )
+
+  ; Types
+  (τ ::=
+     ;; Core calculus
+     (τ_1 → τ_2)
+     α
+     (∀ [α κ] τ)
+     ;; Primitive numeric types
+     bool
+     int8
+     int16
+     int32
+     int64
+     uint8
+     uint16
+     uint32
+     uint64
+     float32
+     float64
+     ;; Tensor types and dimensions
+     (tensor τ [τ ...])
+     number
+     ;; Arithmetic (numbers only)
+     (- τ)
+     (+ τ_1 τ_2)
+     (- τ_1 τ_2)
+     (* τ_1 τ_2)
+     (/ τ_1 τ_2)
+     (abs τ)
+     (min τ)
+     (max τ)
+     ;; Conditionals
+     (if τ_1 τ_2 τ_3)
+     (== τ_1 τ_2)
+     (<= τ_1 τ_2)
+     (>= τ_1 τ_2)
+     (< τ_1 τ_2)
+     (> τ_1 τ_2)
+     )
+
+  ;; Kinds
+  (κ ::=
+     ⋆
+     ℕ
+     )
+
+  ;; Variables
+  (x ::= variable-not-otherwise-mentioned)
+  (α ::= variable-not-otherwise-mentioned)
+  )
