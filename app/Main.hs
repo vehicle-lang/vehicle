6eda6d7f83b205ffef75947eb1fd1394e6e23abd
Wen Kokke <wenkokke@users.noreply.github.com>
2021-03-04 10:49:28 +0000

    Created separate tokens for every function symbol.

--- app/Main.hs
+++ app/Main.hs
@@ -2,13 +2,10 @@ module Main where
 
 import Control.Monad (when)
 import Data.Text (Text)
-import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import System.Environment
-import Vehicle.Frontend.Abs (LIdent(..), Kind(..), Type(..), Expr(..))
 import Vehicle.Frontend.Lex (Token)
-import Vehicle.Frontend.Par (pKind, pType, pExpr, pListDecl, myLexer)
-import Vehicle.Frontend.Print (Print, printTree)
+import Vehicle.Frontend.Par (pProg, myLexer)
 import Vehicle.Frontend.Layout (resolveLayout)
 
 type Parser a = [Token] -> Either String a
@@ -26,7 +23,7 @@ main = do
     error "usage: vehicle [INPUT_FILE]"
   let [file] = args
   contents <- T.readFile file
-  let errOrAst = runParser True pListDecl contents
+  let errOrAst = runParser True pProg contents
   case errOrAst of
     Left  err -> putStrLn err
     Right ast -> print ast
--- test/Spec.hs
+++ test/Spec.hs
@@ -7,7 +7,7 @@ import Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import System.FilePath
-import Vehicle.Frontend.Abs (LIdent(..), Kind(..), Type(..), Expr(..), Bound(..), Decl(..))
+import Vehicle.Frontend.Abs
 import Vehicle.Frontend.Lex (Token)
 import Vehicle.Frontend.Par (pKind, pType, pExpr, pProg, myLexer)
 import Vehicle.Frontend.Print (Print, printTree)
@@ -22,44 +22,7 @@ main = runTestTT . TestList $
 
 parserTests :: Test
 parserTests = TestList
-  [ TestLabel "pKind" (runParserTests False pKind pKindTests)
-  , TestLabel "pType" (runParserTests False pType pTypeTests)
-  , TestLabel "pExpr" (runParserTests False pExpr pExprTests)
-  , TestLabel "examples" (runFileTests pFileTests)
-  ]
-
-pKindTests :: [(Text, Maybe Kind)]
-pKindTests =
-  [ "*"               |-> KStar
-  , "Nat"             |-> KNat
-  , "List *"          |-> KApp KList KStar
-  , "List (List Nat)" |-> KApp KList (KApp KList KNat)
-  ]
-
-pTypeTests :: [(Text, Maybe Type)]
-pTypeTests =
-  [ "Bool"                 |-> TBool
-  , "Real"                 |-> TReal
-  , "Nat"                  |-> TNat
-  , "1312"                 |-> TLitNat 1312
-  , fail "-1"
-  , "[1, 2, 3, 4]"         |-> TListOf [TLitNat 1, TLitNat 2, TLitNat 3, TLitNat 4]
-  , "Cons 1 (Cons 2 Nil)"  |-> TApp (TApp TCons (TLitNat 1)) (TApp (TApp TCons (TLitNat 2)) TNil)
-  , "2 + 2"                |-> TAdd (TLitNat 2) (TLitNat 2)
-  , "Real -> Bool"         |-> TFun TReal TBool
-  , "Real -> Real -> Bool" |-> TFun TReal (TFun TReal TBool)
-  , "a"                    |-> TVar (LIdent ((1, 1),"a"))
-  , "forall x. [x]"        |-> TForall [(LIdent ((1, 8),"x"))] (TListOf [TVar (LIdent ((1, 12),"x"))])
-  ]
-
-pExprTests :: [(Text, Maybe Expr)]
-pExprTests =
-  [ "let y = x in x" |->
-    (ELet [DeclExpr (LIdent ((1,6),"y")) [] (EVar (LIdent ((1,10),"x")))] (EVar (LIdent ((1,16),"x"))))
-  , "x ! 0" |->
-    (EAt (EVar (LIdent ((1,1),"x"))) (ELitNat 0))
-  , "f x ! 0" |->
-    (EAt (EApp (EVar (LIdent ((1,1),"f"))) (EVar (LIdent ((1,3),"x")))) (ELitNat 0))
+  [ TestLabel "examples" (runFileTests pFileTests)
   ]
 
 pFileTests :: [FilePath]
@@ -70,12 +33,6 @@ pFileTests =
   ]
 
 -- ** Helper functions
-
-(|->) :: Text -> b -> (Text, Maybe b)
-x |-> y = (x, Just y)
-
-fail :: Text -> (Text, Maybe b)
-fail x = (x, Nothing)
 
 type Parser a = [Token] -> Either String a
 
