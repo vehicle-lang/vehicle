f0482baf10e97afa7096df8f928b25d287615803
Wen Kokke <wenkokke@users.noreply.github.com>
2021-05-14 21:44:09 +0100

    Added a bit of comments to Check and Infer types.

--- src/hs/Vehicle/Core/Compile/Type.hs
+++ src/hs/Vehicle/Core/Compile/Type.hs
@@ -1,7 +1,6 @@
 {-# LANGUAGE AllowAmbiguousTypes  #-}
 {-# LANGUAGE DataKinds            #-}
 {-# LANGUAGE FlexibleContexts     #-}
-{-# LANGUAGE KindSignatures       #-}
 {-# LANGUAGE LambdaCase           #-}
 {-# LANGUAGE LiberalTypeSynonyms  #-}
 {-# LANGUAGE RankNTypes           #-}
@@ -14,11 +13,10 @@
 
 module Vehicle.Core.Compile.Type where
 
-import           Control.Arrow
 import           Control.Monad.Except (Except, MonadError(..))
-import           Control.Monad.Reader (Reader, ReaderT, MonadReader(..))
-import           Control.Monad.State (State, StateT , MonadState(..))
-import           Control.Monad.Writer (Writer, WriterT, MonadWriter(..))
+import           Control.Monad.Reader (ReaderT, MonadReader(..))
+import           Control.Monad.State (StateT , MonadState(..))
+import           Control.Monad.Writer (WriterT, MonadWriter(..))
 import           Data.Sequence (Seq, (!?))
 import           Vehicle.Core.AST
 import           Vehicle.Core.Compile.DataFlow
@@ -41,8 +39,7 @@ indexOutOfBounds (K p) db =
 -- * Type information
 
 -- |Type information, based on sort.
-newtype Info (sort :: Sort)
-  = I { unI :: INFO sort }
+newtype Info (sort :: Sort) = I { unI :: INFO sort }
 
 -- |Computes type information based on sort; kinds for types, types for expressions.
 type family INFO (sort :: Sort) where
@@ -87,16 +84,30 @@ noInfo = case sortSing @sort of { SKIND -> I (); SDECL -> I (); SPROG -> I () }
 
 -- *
 
-type Check (sort :: Sort) = ReaderT (INFO sort) (Except TypeError)
-type Infer (sort :: Sort) = WriterT (INFO sort) (Except TypeError)
+-- |Type of a checking algorithm.
+--
+-- A computation of the |Check| type returns a tree annotated with typing information or throws an exception.
+-- Furthermore, there are two different types of data threaded through the computation. The |DATAFLOW Ctx|
+-- construct threads a typing context through the computation, as appropriate for the sort we're checking,
+-- e.g., expression have read and /local/ write access to the context. The |ReaderT (INFO sort)| construct
+-- provides the information to check against.
+--
+type Check (sort :: Sort)
+  = (DATAFLOW Ctx)
+    (ReaderT (INFO sort) (Except TypeError))
+    (Tree DeBruijn Builtin (Info :*: K Provenance))
+    sort
 
-type CHECKINFER (sort :: Sort)
-  = ( (DATAFLOW Ctx) (Check sort) (Tree DeBruijn Builtin (Info :*: K Provenance)) sort
-    , (DATAFLOW Ctx) (Infer sort) (Tree DeBruijn Builtin (Info :*: K Provenance)) sort
-    )
+-- |Type of an inference algorithm.
+--
+-- A computation of the |Infer| type is similar to the |Check| type, but instead of /requiring/ the
+-- information to check against via a reader monad, it /provides/ said information via a writer monad.
+type Infer (sort :: Sort)
+  = (DATAFLOW Ctx) (WriterT (INFO sort) (Except TypeError)) (Tree DeBruijn Builtin (Info :*: K Provenance)) sort
 
-newtype CheckInfer (sort :: Sort)
-  = CI { unCI :: CHECKINFER sort }
+type CHECKINFER (sort :: Sort) = (Check sort, Infer sort)
+
+newtype CheckInfer (sort :: Sort) = CI { unCI :: CHECKINFER sort }
 
 checkInferF ::
   forall sort.
@@ -107,11 +118,18 @@ checkInferF ::
 checkInferF = case sortSing @sort of
 
   SKIND -> \case
-    KAppF  ann k1 k2 -> _
-    KConF  ann op    -> return &&& return $ KCon  (noInfo :*: ann) op
-    KMetaF ann i     -> return &&& return $ KMeta (noInfo :*: ann) i
+    KAppF  ann k1 k2 -> KApp (noInfo :*: ann) <$$> unCI k1 <**> unCI k2
+    KConF  ann op    -> return2 $ KCon  (noInfo :*: ann) op
+    KMetaF ann i     -> return2 $ KMeta (noInfo :*: ann) i
 
-  STYPE -> undefined
+  STYPE -> \case
+    TForallF  ann n t   -> _
+    TAppF     ann t1 t2 -> _
+    TVarF     ann n     -> _
+    TConF     ann op    -> _
+    TLitDimF  ann d     -> _
+    TLitListF ann ts    -> _
+    TMetaF    ann i     -> _
 
   STARG -> undefined
 
@@ -122,6 +140,18 @@ checkInferF = case sortSing @sort of
   SDECL -> undefined
 
   SPROG -> undefined
+
+
+-- * Helper functions
+
+return2 :: (Monad m1, Monad m2) => a -> (m1 a, m2 a)
+return2 x = (return x, return x)
+
+(<$$>) :: (Functor f1, Functor f2) => (a -> b) -> (f1 a, f2 a) -> (f1 b, f2 b)
+(<$$>) f (x, y) = (f <$> x, f <$> y)
+
+(<**>) :: (Applicative f1, Applicative f2) => (f1 (a -> c), f2 (b -> d)) -> (f1 a, f2 b) -> (f1 c, f2 d)
+(<**>) (f, g) (a, b) = (f <*> a, g <*> b)
 
 -- -}
 -- -}
