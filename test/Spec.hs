ba847699357245ecf5387bdbc2d4bd21aa22c119
= <matthewdaggitt@gmail.com>
2021-07-08 21:52:29 +0800

    Added test for compilation

--- test/Spec.hs
+++ test/Spec.hs
@@ -3,31 +3,57 @@ import Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import Test.HUnit
+import System.Exit (exitFailure)
+
 import Vehicle.Frontend.Parse (parseFile)
+import Vehicle.Core.AST as VC
+import Vehicle.Frontend.AST as VF
+import Vehicle.Error
+import Vehicle.Core.Compile (compile)
+import Vehicle.Frontend.Elaborate
 
 main :: IO Counts
 main = runTestTT . TestList $
   [ TestLabel "parser" parserTests
+  , TestLabel "compile" compileTests
   ]
 
 -- * Test parser for Vehicle Frontend
 
-parserTests :: Test
-parserTests = TestList
-  [ TestLabel "examples" (runFileTests pFileTests)
-  ]
-
-pFileTests :: [FilePath]
-pFileTests =
+fileTests :: [FilePath]
+fileTests =
   [ "examples/andGate.vcl"
   , "examples/mnist.vcl"
   , "examples/shortestPath.vcl"
   ]
 
+parserTests :: Test
+parserTests = TestList
+  [ TestLabel "examples" (runParseFileTests fileTests)
+  ]
+
+compileTests :: Test
+compileTests = TestList
+  [ TestLabel "examples" (runCompileFileTests fileTests)
+  ]
+
 -- ** Helper functions
 
-runFileTests :: [FilePath] -> Test
-runFileTests files = TestList [ TestLabel file (TestCase (void $ parseFile file)) | file <- files ]
+runParseFileTests :: [FilePath] -> Test
+runParseFileTests files = TestList [ TestLabel file (TestCase (void $ parseFile file)) | file <- files ]
+
+runCompileFileTests :: [FilePath] -> Test
+runCompileFileTests files = TestList [ TestLabel file (TestCase (void $ parseAndCompile file)) | file <- files]
+
+parseAndCompile :: FilePath -> IO VC.OutputProg
+parseAndCompile filePath = do
+  prog <- parseFile filePath
+  coreProg <- fromEitherIO $ runElab (elab prog)
+  fromEitherIO $ compile coreProg
+
+fromEitherIO :: MeaningfulError e => Either e a -> IO a
+fromEitherIO (Left err) = do print (details err); exitFailure
+fromEitherIO (Right x)  = return x
 
 rightToMaybe :: Either a b -> Maybe b
 rightToMaybe (Left  _) = Nothing
