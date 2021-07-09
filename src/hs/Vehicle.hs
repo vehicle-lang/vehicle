afc7ae431ff3583a942808102f2dee200affcc43
= <matthewdaggitt@gmail.com>
2021-07-09 11:05:06 +0800

    Moved most of `Main` into new `Vehicle` module so that it can be exposed for tests

---
+++ src/hs/Vehicle.hs
@@ -0,0 +1,197 @@
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Vehicle
+  ( VehicleLang(..)
+  , ITP(..)
+  , Solver(..)
+  , OutputTarget(..)
+  , Options(..)
+  , defaultOptions
+  , parseAndRun
+  , run
+  ) where
+
+import Paths_vehicle (version)
+
+import Control.Monad (when)
+import Data.Text (Text)
+import Data.Char (toLower)
+import Data.Text.IO qualified as T
+import System.Environment (getArgs)
+import System.Exit (exitSuccess, exitFailure)
+import System.Console.GetOpt
+
+import Vehicle.Prelude ((|->) , (!?))
+import Vehicle.Core.AST qualified as VC
+import Vehicle.Core.Parse qualified as VC
+import Vehicle.Core.Print qualified as VC
+import Vehicle.Core.Compile qualified as VC
+import Vehicle.Frontend.Print qualified as VF
+import Vehicle.Frontend.Elaborate qualified as VF
+import Vehicle.Frontend.Delaborate qualified as VF
+import Vehicle.Frontend.Parse qualified as VF
+import Vehicle.Backend.ITP.Core (ITPOptions(..))
+import Vehicle.Backend.ITP.Agda (AgdaOptions(..), compileToAgda)
+import Vehicle.Error qualified as V
+
+
+--------------------------------------------------------------------------------
+-- Command-line options
+
+data VehicleLang = Frontend | Core
+
+data ITP = Agda
+
+data Solver
+
+data OutputTarget
+  = ITP ITP
+  | Solver Solver
+  | Vehicle VehicleLang
+
+data Options = Options
+  { showHelp     :: Bool
+  , showVersion  :: Bool
+  , inputLang    :: VehicleLang
+  , inputFile    :: Maybe FilePath
+  , outputTarget :: Maybe OutputTarget
+  , outputFile   :: Maybe FilePath
+  }
+
+defaultOptions :: Options
+defaultOptions = Options
+  { showHelp     = False
+  , showVersion  = False
+  , inputFile    = Nothing
+  , inputLang    = Frontend
+  , outputTarget = Nothing
+  , outputFile   = Nothing
+  }
+
+options :: [OptDescr (Options -> Options)]
+options =
+  [ Option ['h'] ["help"]
+    (NoArg (\opts -> opts { showHelp = True }))
+    "Show help information."
+
+  , Option ['v'] ["version"]
+    (NoArg (\opts -> opts { showVersion = True }))
+    "Show version information."
+
+  ,  Option ['i'] ["input-file"]
+    (ReqArg (\arg opts -> opts { inputFile = Just arg }) "FILE")
+    "Input file."
+
+  , Option [] ["core"]
+    (NoArg (\opts -> opts { inputLang = Core }))
+    "Set input language to Vehicle Core."
+
+  , Option ['t'] ["target"]
+    (ReqArg (\arg opts -> opts { outputTarget = parseOutputTarget arg}) "OutputTarget")
+    "Compilation target."
+
+  , Option ['o'] ["output-file"]
+    (ReqArg (\arg opts -> opts { outputTarget = parseOutputTarget arg}) "FILE")
+    "Output file."
+  ]
+
+usageHeader :: String
+usageHeader = "Usage: vehicle [OPTION...]\n"
+
+usageInstructions :: String
+usageInstructions = usageInfo usageHeader options
+
+parseOpts :: [String] -> IO (Options, [String])
+parseOpts argv =
+  case getOpt Permute options argv of
+    (o,n,[]  ) -> return (foldl (flip id) defaultOptions o, n)
+    (_,_,errs) -> ioError (userError (concat errs ++ usageInstructions))
+
+parseOutputTarget :: String -> Maybe OutputTarget
+parseOutputTarget s = outputTargets !? map toLower s
+  where
+    outputTargets =
+      [ "agda"         |-> ITP Agda
+      , "vehicle-core" |-> Vehicle Core
+      , "vehicle"      |-> Vehicle Frontend
+      ]
+
+--------------------------------------------------------------------------------
+-- Main function
+
+parseAndRun :: IO ()
+parseAndRun = do
+  (opts, _args) <- parseOpts =<< getArgs
+  run opts
+
+run :: Options -> IO ()
+run _opts@Options{..} = do
+  -- Print usage information and exit
+  when (showVersion || showHelp) $ do
+    when showVersion $ do
+      print version
+    when showHelp $ do
+      putStrLn usageInstructions
+    exitSuccess
+
+  -- Read file, parse and elaborate to core if necessary
+  contents <- readFileOrStdin inputFile
+  coreProg <- parseAndElab inputLang contents
+
+  T.putStrLn (VC.printTree coreProg)
+
+  -- Scope check, type check etc.
+  compCoreProg <- fromEitherIO $ VC.compile coreProg
+
+  T.putStrLn (VC.printTree compCoreProg)
+
+  -- Compile to requested backend
+  case outputTarget of
+    Nothing -> do
+      putStrLn "Please specify an output target with -t or --target"
+      exitFailure
+
+    Just (ITP itp) -> do
+      compFrontProg <- fromEitherIO $ VF.runDelab compCoreProg
+
+      outputText <- case itp of
+        Agda -> do
+          let itpOptions = ITPOptions
+                { vehicleUIDs  = mempty
+                , aisecVersion = version
+                , backendOpts  = AgdaOptions
+                  { useProp    = False
+                  , modulePath = ["Testing"]
+                  }
+                }
+          fromEitherIO $ compileToAgda itpOptions compFrontProg
+
+      case outputFile of
+        Nothing             -> T.putStrLn outputText
+        Just outputFilePath -> T.writeFile outputFilePath outputText
+
+    Just (Solver v) -> do
+      putStrLn "No solvers implemented yet"
+      exitFailure
+
+    Just (Vehicle Core) -> T.putStrLn (VC.printTree compCoreProg)
+    Just (Vehicle Frontend) -> do
+      compFrontProg <- fromEitherIO $ VF.runDelab compCoreProg
+      T.putStrLn (VF.printTree compFrontProg)
+
+parseAndElab :: VehicleLang -> Text -> IO VC.InputProg
+parseAndElab Frontend contents = do
+  progVF <- fromEitherIO (VF.parseText contents)
+  fromEitherIO (VF.runElab (VF.elab progVF))
+parseAndElab Core contents =
+  fromEitherIO (VC.parseText contents)
+
+fromEitherIO :: V.MeaningfulError e => Either e a -> IO a
+fromEitherIO (Left err) = do print (V.details err); exitFailure
+fromEitherIO (Right x)  = return x
+
+readFileOrStdin :: Maybe FilePath -> IO Text
+readFileOrStdin (Just file) = T.readFile file
+readFileOrStdin Nothing = T.getContents
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+
 import Control.Monad (void)
 import Data.Text (Text)
 import qualified Data.Text as T
@@ -5,17 +7,12 @@ import qualified Data.Text.IO as T
 import Test.HUnit
 import System.Exit (exitFailure)
 
-import Vehicle.Frontend.Parse (parseFile)
-import Vehicle.Core.AST as VC
-import Vehicle.Frontend.AST as VF
-import Vehicle.Error
-import Vehicle.Core.Compile (compile)
-import Vehicle.Frontend.Elaborate
+import Vehicle
 
 main :: IO Counts
 main = runTestTT . TestList $
-  [ TestLabel "parser" parserTests
-  , TestLabel "compile" compileTests
+  [ TestLabel "agda" agdaTests
+  --, TestLabel "AA" compileTests
   ]
 
 -- * Test parser for Vehicle Frontend
@@ -26,35 +23,35 @@ fileTests =
   , "examples/mnist.vcl"
   , "examples/shortestPath.vcl"
   ]
-
+{-
 parserTests :: Test
 parserTests = TestList
   [ TestLabel "examples" (runParseFileTests fileTests)
   ]
+-}
 
-compileTests :: Test
-compileTests = TestList
-  [ TestLabel "examples" (runCompileFileTests fileTests)
+agdaTests :: Test
+agdaTests = TestList
+  [ TestLabel "examples" (runAgdaFileTests fileTests)
   ]
 
 -- ** Helper functions
 
-runParseFileTests :: [FilePath] -> Test
-runParseFileTests files = TestList [ TestLabel file (TestCase (void $ parseFile file)) | file <- files ]
+runFileTests :: (String -> IO a) -> [FilePath] -> Test
+runFileTests test files = TestList [ TestLabel file (TestCase (void $ test file)) | file <- files ]
 
-runCompileFileTests :: [FilePath] -> Test
-runCompileFileTests files = TestList [ TestLabel file (TestCase (void $ parseAndCompile file)) | file <- files]
+-- runParseFileTests :: [FilePath] -> Test
+-- runParseFileTests = runFileTests parseFile
 
-parseAndCompile :: FilePath -> IO VC.OutputProg
-parseAndCompile filePath = do
-  prog <- parseFile filePath
-  coreProg <- fromEitherIO $ runElab (elab prog)
-  fromEitherIO $ compile coreProg
+runAgdaFileTests :: [FilePath] -> Test
+runAgdaFileTests = runFileTests compileToAgda
 
-fromEitherIO :: MeaningfulError e => Either e a -> IO a
-fromEitherIO (Left err) = do print (details err); exitFailure
-fromEitherIO (Right x)  = return x
-
-rightToMaybe :: Either a b -> Maybe b
-rightToMaybe (Left  _) = Nothing
-rightToMaybe (Right x) = Just x
+compileToAgda :: FilePath -> IO ()
+compileToAgda filePath = run $ Options
+  { showHelp     = False
+  , showVersion  = False
+  , inputLang    = Frontend
+  , inputFile    = Just filePath
+  , outputTarget = Just $ Vehicle Frontend
+  , outputFile   = Nothing
+  }
