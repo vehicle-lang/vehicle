0ca62afe3ce029e5f6b95ae08455c249508d7195
Wen Kokke <wenkokke@users.noreply.github.com>
2021-11-30 14:59:36 +0000

    Refactored printing

--- src/hs/Vehicle/Language/Print.hs
+++ src/hs/Vehicle/Language/Print.hs
@@ -1,9 +1,9 @@
 {-# OPTIONS_GHC -Wno-orphans #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE  #-}
 
 module Vehicle.Language.Print
-  ( PrettyLang(..)
-  , PrettyDescopedLang(..)
-  , prettySimple
+  ( prettySimple
   , prettyVerbose
   , prettyFriendly
   , prettyFriendlyDB
@@ -14,7 +14,7 @@ import Prettyprinter (list, tupled)
 
 import Data.Text
 
-import Vehicle.Core.Print as Core (printTree)
+import Vehicle.Core.Print as Core (printTree, Print)
 import Vehicle.Frontend.Print as Frontend (printTree)
 import Vehicle.Core.Abs qualified as BC
 import Vehicle.Frontend.Abs qualified as BF
@@ -29,6 +29,112 @@ import Vehicle.Compile.SupplyNames (SupplyNames, runSupplyNamesWithCtx, runSuppl
 import Vehicle.Compile.Type.MetaSubstitution
 import Vehicle.Compile.Type.Constraint
 
+
+-- |Prints to the core language removing all implicit/instance arguments and
+-- automatically inserted code. Does not convert DeBruijn indices back to names.
+prettySimple :: PrettyWith ('Simple 'Core) a => a -> Doc b
+prettySimple = prettyWith
+
+-- |Prints to the core language in all it's gory detail. Does not convert DeBruijn
+-- indices back to names. Useful for debugging.
+prettyVerbose :: PrettyWith 'Core a => a -> Doc b
+prettyVerbose = prettyWith
+
+-- |Prints to the frontend language for things that need to be displayed to
+-- the user.
+prettyFriendly :: PrettyWith ('Simple ('Named 'Frontend)) a => a -> Doc b
+prettyFriendly = prettyWith
+
+-- |Prints to the frontend language for things that need to be displayed to
+-- the user. Use this when the expression is using DeBruijn indices and is
+-- not closed.
+prettyFriendlyDB :: PrettyWith ('Simple ('Named ('With [Maybe Symbol] 'Frontend))) ([Maybe Symbol], t (Maybe Symbol) LocallyNamelessVar ann)
+                 => [Maybe Symbol]
+                 -> t (Maybe Symbol) LocallyNamelessVar ann
+                 -> Doc b
+prettyFriendlyDB ctx e = prettyWith (ctx, e)
+
+-- | This is identical to |prettyFriendly|, but exists for historical reasons.
+prettyFriendlyDBClosed :: (PrettyWith ('Simple ('Named 'Frontend))) => a -> Doc b
+prettyFriendlyDBClosed = prettyWith
+
+data Tags
+  = Core
+  | Frontend
+  | Named Tags
+  | Simple Tags
+  | With (arg :: *) Tags
+
+class PrettyWith (tags :: Tags) a where
+  prettyWith :: forall tags. a -> Doc b
+
+
+-- Core printing functions, via the BNFC grammars
+
+newtype ViaBnfc a = ViaBnfc a
+
+instance (Core.Delaborate a b, Core.Print b) => PrettyWith 'Core (ViaBnfc a) where
+  prettyWith (ViaBnfc e) = pretty (Core.printTree (Core.delab e))
+
+instance (Frontend.Delaborate a b, Frontend.Print b) => PrettyWith 'Front (ViaBnfc a) where
+  prettyWith (ViaBnfc e) = pretty (Frontend.printTree (Frontend.delab e))
+
+deriving via (ViaBnfc (NamedProg ann)) instance PrettyWith 'Core (NamedProg ann)
+deriving via (ViaBnfc (NamedDecl ann)) instance PrettyWith 'Core (NamedDecl ann)
+deriving via (ViaBnfc (NamedExpr ann)) instance PrettyWith 'Core (NamedExpr ann)
+
+deriving via (ViaBnfc (NamedProg ann)) instance PrettyWith 'Frontend (NamedProg ann)
+deriving via (ViaBnfc (NamedDecl ann)) instance PrettyWith 'Frontend (NamedDecl ann)
+deriving via (ViaBnfc (NamedExpr ann)) instance PrettyWith 'Frontend (NamedExpr ann)
+
+
+-- Converting from deBruijn indices back to names
+
+-- | An instance for named where the argument is already named.
+instance (PrettyWith tags (t Symbol Symbol ann))
+      => PrettyWith ('Named tags) (t Symbol Symbol ann) where
+  prettyWith e = e
+
+-- | An instance for named where the argument needs to be descoped, but is assumed to be a closed term.
+instance (SupplyNames t, Descope t, PrettyWith tags (t Symbol Symbol ann))
+      => PrettyWith ('Named tags) (t Symbol LocallyNamelessVar ann) where
+  prettyWith e = prettyWith (runDescope mempty (runSupplyNames e))
+
+instance (SupplyNames t, Descope t, PrettyWith tags (t Symbol Symbol ann))
+      => PrettyWith ('Named ('With [Maybe Symbol] tags)) ([Maybe Symbol], t Symbol LocallyNamelessVar ann) where
+  prettyWith p = prettyWith (uncurry runDescope (runSupplyNamesWithCtx p))
+
+
+-- Printing deBruijn indices
+
+newtype ViaNaiveDescope a = ViaNaiveDescope a
+
+instance (Descope t, PrettyWith tags (t Symbol Symbol ann))
+      => PrettyWith tags (ViaNaiveDescope (t (Maybe Symbol) LocallyNamelessVar ann)) where
+  prettyWith e = prettyWith (runNaiveDescope e)
+
+deriving via (ViaNaiveDescope (DeBruijnDecl ann)) instance PrettyWith 'Core (DeBruijnDecl ann)
+deriving via (ViaNaiveDescope (DeBruijnProg ann)) instance PrettyWith 'Core (DeBruijnProg ann)
+deriving via (ViaNaiveDescope (DeBruijnExpr ann)) instance PrettyWith 'Core (DeBruijnExpr ann)
+
+deriving via (ViaNaiveDescope (DeBruijnProg ann)) instance PrettyWith 'Frontend (DeBruijnProg ann)
+deriving via (ViaNaiveDescope (DeBruijnDecl ann)) instance PrettyWith 'Frontend (DeBruijnDecl ann)
+deriving via (ViaNaiveDescope (DeBruijnExpr ann)) instance PrettyWith 'Frontend (DeBruijnExpr ann)
+
+
+-- Optional simplification
+
+instance (Simplify a, PrettyWith tags a)
+      => PrettyWith ('Simple tags) a where
+  prettyWith e = prettyWith (simplify e)
+
+instance (Simplify a, PrettyWith tags a)
+      => PrettyWith ('Simple ('With SimplifyOptions tags)) (SimplifyOptions, a) where
+  prettyWith p = prettyWith (uncurry simplifyWith p)
+
+-- etc...
+
+{-
 --------------------------------------------------------------------------------
 -- Top-level interface for printing Vehicle expressions/programs
 
@@ -178,3 +284,5 @@ instance PrettyLang BaseConstraint where
 
 instance PrettyLang Constraint where
   prettyLang target (Constraint _ c) = prettyLang target c
+
+-- -}
