bc23424109dc9cded3c89ca0b00435670f148f2b
= <matthewdaggitt@gmail.com>
2021-12-03 14:57:47 +0000

    Renabled BNFC printing hack using Text.replace

--- src/hs/Vehicle/Language/Print.hs
+++ src/hs/Vehicle/Language/Print.hs
@@ -13,6 +13,9 @@ module Vehicle.Language.Print
   ) where
 
 import GHC.TypeLits (TypeError, ErrorMessage(..))
+import Data.Text (Text)
+import Data.Text qualified as Text
+import Prettyprinter (list, tupled)
 
 import Vehicle.Core.Print as Core (printTree, Print)
 import Vehicle.Frontend.Print as Frontend (printTree, Print)
@@ -26,7 +29,6 @@ import Vehicle.Compile.Delaborate.Core as Core
 import Vehicle.Compile.Delaborate.Frontend as Frontend
 import Vehicle.Compile.Descope
 import Vehicle.Compile.SupplyNames (SupplyNames, runSupplyNamesWithCtx, runSupplyNames)
-import Prettyprinter (list, tupled)
 import Vehicle.Compile.Type.Constraint (Constraint (..), BaseConstraint (..))
 import Vehicle.Compile.Type.MetaSubstitution (MetaSubstitution(MetaSubstitution))
 
@@ -198,7 +200,7 @@ instance PrettyUsing rest CheckedExpr
 newtype ViaBnfcCore a = ViaBnfcCore a
 
 instance Core.Print a => Pretty (ViaBnfcCore a) where
-  pretty (ViaBnfcCore e) = pretty (Core.printTree e)
+  pretty (ViaBnfcCore e) = pretty $ bnfcPrintHack (Core.printTree e)
 
 deriving via (ViaBnfcCore BC.Prog) instance Pretty BC.Prog
 deriving via (ViaBnfcCore BC.Decl) instance Pretty BC.Decl
@@ -207,162 +209,16 @@ deriving via (ViaBnfcCore BC.Expr) instance Pretty BC.Expr
 newtype ViaBnfcFrontend a = ViaBnfcFrontend a
 
 instance Frontend.Print a => Pretty (ViaBnfcFrontend a) where
-  pretty (ViaBnfcFrontend e) = pretty (Frontend.printTree e)
+  pretty (ViaBnfcFrontend e) = pretty $ bnfcPrintHack (Frontend.printTree e)
 
 deriving via (ViaBnfcFrontend BF.Prog) instance Pretty BF.Prog
 deriving via (ViaBnfcFrontend BF.Decl) instance Pretty BF.Decl
 deriving via (ViaBnfcFrontend BF.Expr) instance Pretty BF.Expr
 
-{-
---------------------------------------------------------------------------------
--- Top-level interface for printing Vehicle expressions/programs
-
--- |Prints to the core language removing all implicit/instance arguments and
--- automatically inserted code. Does not convert DeBruijn indices back to names.
-prettySimple :: (PrettyLang a, Simplify a) => a -> Doc b
-prettySimple = prettyLang Core . runSimplify options
-  where
-    options = SimplifyOptions
-      { removeImplicits   = True
-      , removeInstances   = True
-      , removeNonUserCode = True
-      }
-
--- |Prints to the core language in all it's gory detail. Does not convert DeBruijn
--- indices back to names. Useful for debugging.
-prettyVerbose :: (PrettyLang a, Simplify a) => a -> Doc b
-prettyVerbose = prettyLang Core . runSimplify options
-  where
-    options = SimplifyOptions
-      { removeImplicits   = False
-      , removeInstances   = False
-      , removeNonUserCode = False
-      }
-
--- |Prints to the frontend language for things that need to be displayed to
--- the user. Use this when not using DeBruijn indices.
-prettyFriendly :: (PrettyLang a, Simplify a) => a -> Doc b
-prettyFriendly = prettyLang Frontend . runSimplify options
-  where
-    options = SimplifyOptions
-      { removeImplicits   = False
-      , removeInstances   = False
-      , removeNonUserCode = False
-      }
-
--- |Prints to the frontend language for things that need to be displayed to
--- the user. Use this when the expression is using DeBruijn indices
-prettyFriendlyDB :: ( PrettyDescopedLang t binder var ann
-                  , Simplify (t binder var ann))
-                 => [binder]
-                 -> t binder var ann
-                 -> Doc b
-prettyFriendlyDB ctx = prettyDescopeLang Frontend ctx . runSimplify options
-  where
-    options = SimplifyOptions
-      { removeImplicits   = True
-      , removeInstances   = True
-      , removeNonUserCode = False
-      }
-
--- |Use for closed terms using DeBruijn indices
-prettyFriendlyDBClosed :: (PrettyDescopedLang t binder var ann
-                        , Simplify (t binder var ann))
-                     => t binder var ann
-                     -> Doc b
-prettyFriendlyDBClosed = prettyFriendlyDB mempty
-
---------------------------------------------------------------------------------
--- Printing when using names rather than DeBruijn indices
-
-class PrettyLang a where
-  prettyLang :: VehicleLang -> a -> Doc b
-
-instance PrettyLang a => PrettyLang [a] where
-  prettyLang target xs = list (fmap (prettyLang target) xs)
-
-instance (Pretty a, PrettyLang b) => PrettyLang (a, b) where
-  prettyLang target (x, y) = tupled [pretty x, prettyLang target y]
-
-instance PrettyNamedLang t binder var ann => PrettyLang (t binder var ann) where
-  prettyLang = prettyNamedLang
-
---------------------------------------------------------------------------------
--- Printing when using names
-
-class PrettyNamedLang t binder var ann where
-  prettyNamedLang :: VehicleLang -> t binder var ann -> Doc b
-
-instance PrettyNamedLang Arg  Symbol Symbol ann where
-  prettyNamedLang Core     e = pretty $ bnfcPrintHack $ Core.printTree (Core.runDelabWithoutLogging e :: BC.Arg)
-  prettyNamedLang Frontend e = pretty $ bnfcPrintHack $ Frontend.printTree (Frontend.runDelabWithoutLogging e :: BF.Arg)
-
-instance PrettyNamedLang Expr Symbol Symbol ann where
-  prettyNamedLang Core     e = pretty $ bnfcPrintHack $ Core.printTree (Core.runDelabWithoutLogging e :: BC.Expr)
-  prettyNamedLang Frontend e = pretty $ bnfcPrintHack $ Frontend.printTree (Frontend.runDelabWithoutLogging e :: BF.Expr)
-
-instance PrettyNamedLang Decl Symbol Symbol ann where
-  prettyNamedLang Core     e = pretty $ bnfcPrintHack $ Core.printTree (Core.runDelabWithoutLogging e :: BC.Decl)
-  prettyNamedLang Frontend e = pretty $ bnfcPrintHack $ Frontend.printTree (Frontend.runDelabWithoutLogging e :: [BF.Decl])
-
-instance PrettyNamedLang Prog Symbol Symbol ann where
-  prettyNamedLang Core     e = pretty $ bnfcPrintHack $ Core.printTree (Core.runDelabWithoutLogging e :: BC.Prog)
-  prettyNamedLang Frontend e = pretty $ bnfcPrintHack $ Frontend.printTree (Frontend.runDelabWithoutLogging e :: BF.Prog)
-
-instance ( SupplyNames t
-         , PrettyNamedLang t Symbol var ann
-         ) => PrettyNamedLang t (Maybe Symbol) var ann where
-  prettyNamedLang target e = prettyNamedLang target (runSupplyNames e)
-
--- A hack to that simply converts DeBruijn indices into their corresponding
--- counterpart, i.e. 0 -> "i0"
-instance ( Descope t
-         , PrettyNamedLang t Symbol Symbol ann
-         ) => PrettyNamedLang t Symbol LocallyNamelessVar ann where
-  prettyNamedLang target e = prettyLang target (runNaiveDescope e)
-
---------------------------------------------------------------------------------
--- Printing when using DeBruijn indices
-
-class PrettyDescopedLang t binder var ann where
-  prettyDescopeLang :: VehicleLang -> [binder] -> t binder var ann -> Doc b
-
--- DeBruijn indices with machine names
-instance ( SupplyNames t
-         , Descope t
-         , PrettyNamedLang t Symbol Symbol ann )
-         => PrettyDescopedLang t (Maybe Symbol) LocallyNamelessVar ann where
-  prettyDescopeLang target ctx e = prettyDescopeLang target ctx' e'
-    where (ctx', e') = runSupplyNamesWithCtx (ctx, e)
-
-instance PrettyNamedLang t Symbol Symbol ann
-      => PrettyDescopedLang t Symbol Symbol ann where
-  prettyDescopeLang target _ctx = prettyNamedLang target
-
--- DeBruijn indices without machine names
-instance (PrettyNamedLang t Symbol Symbol ann, Descope t)
-      => PrettyDescopedLang t Symbol LocallyNamelessVar ann where
-  prettyDescopeLang target ctx e = prettyDescopeLang target ctx (runDescope ctx e)
-
---------------------------------------------------------------------------------
--- Other
-
 -- BNFC printer treats the braces for implicit arguments as layout braces and
 -- therefore adds a ton of newlines everywhere. This hack attempts to undo this.
 bnfcPrintHack :: String -> Text
-bnfcPrintHack = pack
-  -- replaceAll "\\{\\s*" "{" $
-  -- replaceAll "\\s*\\}\\s*" "} " $
-
-instance PrettyLang MetaSubstitution where
-  prettyLang target (MetaSubstitution m) = pretty $ fmap (prettyLang target) m
-
-instance PrettyLang BaseConstraint where
-  prettyLang target (Unify (e1, e2)) = prettyLang target e1 <+> "~" <+> prettyLang target e2
-  prettyLang target (m `Has` e)      = pretty m <+> "~" <+> prettyLang target e
-
-instance PrettyLang Constraint where
-  prettyLang target (Constraint _ c) = prettyLang target c
-
--- -}
--- -}
+bnfcPrintHack =
+  Text.replace "{\n" "{" .
+  Text.replace "\n}\n" "} " .
+  Text.pack
