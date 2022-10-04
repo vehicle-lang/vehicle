a9a1d0d738d5398a300e0c203f8e7f58ac8e8954
nslusarz <nds1@hw.ac.uk>
2022-10-04 09:57:04 +0100

    fixing free variable, adding Declarations

--- src/hs/Vehicle/Backend/LossFunction/Compile.hs
+++ src/hs/Vehicle/Backend/LossFunction/Compile.hs
@@ -14,7 +14,19 @@ import Vehicle.Resource.NeuralNetwork
 import Vehicle.Language.AST qualified as V
 import Vehicle.Compile.Prelude qualified as V
 import Vehicle.Language.Print
+import Vehicle.Language.AST.Name (HasName(nameOf))
 
+--------------------------------------------------------------------------------
+-- Declaration definition
+
+data LDecl
+  = DefFunction
+    Symbol                   -- Bound function name.
+    LExpr                    -- Bound function body.
+  deriving (Eq, Show, Generic)
+
+instance FromJSON LDecl
+instance ToJSON LDecl
 --------------------------------------------------------------------------------
 -- Definitions
 
@@ -25,15 +37,15 @@ data Quantifier
   deriving (Eq, Ord, Generic, Show)
 
 instance FromJSON Quantifier
-instance ToJSON Quantifier where
-  toEncoding = genericToEncoding defaultOptions
+instance ToJSON Quantifier
 
 newtype Domain = Domain ()
   deriving (Eq, Ord, Generic, Show)
 
 instance FromJSON Domain
-instance ToJSON Domain where
-  toEncoding = genericToEncoding defaultOptions
+instance ToJSON Domain 
+
+--definitoon of the LExpr - all expressions allowed in loss constraint
 
 data LExpr
   = Negation LExpr
@@ -43,6 +55,7 @@ data LExpr
   | Subtraction LExpr LExpr
   | IndicatorFunction LExpr LExpr
   | Variable V.DBIndex
+  | FreeVariable Symbol
   | NetworkApplication Symbol (NonEmpty LExpr)
   | Quantifier Quantifier Symbol Domain LExpr
   | At LExpr LExpr
@@ -51,23 +64,25 @@ data LExpr
   deriving (Eq, Ord, Generic, Show)
 
 instance FromJSON LExpr
-instance ToJSON LExpr where
-  toEncoding = genericToEncoding defaultOptions
+instance ToJSON LExpr
 
 --------------------------------------------------------------------------------
 -- Compilation
+-- the translation into the LExpr
 
-compile :: MonadCompile m => NetworkCtx -> V.CheckedProg -> m [LExpr]
+compile :: MonadCompile m => NetworkCtx -> V.CheckedProg -> m [LDecl]
 compile _ (V.Main ds) = catMaybes <$> traverse compileDecl ds
 
-compileDecl :: MonadCompile m => V.CheckedDecl -> m (Maybe LExpr)
+compileDecl :: MonadCompile m => V.CheckedDecl -> m (Maybe LDecl)
 compileDecl V.DefResource{} = normalisationError currentPass "Resource declarations"
-compileDecl (V.DefFunction _p _ t expr) =
+compileDecl (V.DefFunction _p i t expr) =
     if not $ V.isProperty t
       -- If it's not a property then we can discard it as all applications
       -- of it should have been normalised out by now.
       then return Nothing
-      else Just <$> compileExpr expr
+      else do 
+        expr' <- compileExpr expr
+        return (Just (DefFunction (nameOf i) expr'))
 
 currentPass :: Doc a
 currentPass = "compilation to loss functions"
@@ -102,6 +117,7 @@ compileExpr e = showExit $ do
     V.LiteralExpr _ _ l                   -> return $ Constant (compileLiteral l)
     V.App _ (V.Var _ (V.Free ident)) p    -> NetworkApplication (V.nameOf ident) <$> traverse compileArg p
     V.Var _ (V.Bound t)                   -> return (Variable t)
+    V.Var _ (V.Free t)                    -> return (FreeVariable (V.nameOf t))
     V.QuantifierExpr q _ binder body      -> Quantifier (compileQuant q) (V.getBinderSymbol binder) (Domain ()) <$> compileExpr body
     V.AtExpr _ _ _ _ [xs, i]              -> At <$> compileArg xs <*> compileArg i
     V.LSeq _ _ xs                         -> TensorLiteral <$> traverse compileExpr xs
