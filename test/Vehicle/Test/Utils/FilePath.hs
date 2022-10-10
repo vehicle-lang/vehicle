996cf2d63b0bf5bd94e71ff0587346ccb794b414
MatthewDaggitt <matthewdaggitt@gmail.com>
2022-10-10 18:34:42 +0100

    Fix #170 by refining filepath regex

--- test/Vehicle/Test/Utils/FilePath.hs
+++ test/Vehicle/Test/Utils/FilePath.hs
@@ -20,10 +20,12 @@ removeFilePaths x =
 filepathRegex :: String
 filepathRegex = drive <> folders <> fileName
   where
-    drive      = "([A-Z]:)?"
-    folder     = "([a-zA-Z\\d]+[\\//])"
-    folders    = "[\\//]?" <> folder <> "+" <> "[a-zA-Z\\d]*"
-    fileName   = "(\\.[a-zA-Z\\d]+)?"
+    drive      = "(([A-Z]:)?[\\//])?"
+    name       = "[a-zA-Z\\d$_-]+" -- Note hyphen must be the last character in the class
+    extension  = "[a-zA-Z\\d]+"
+    folder     = "(" <> name <> "[\\//])"
+    folders    = folder <> "+"
+    fileName   = name <> "(\\." <> extension <> ")?"
 
 sliceOut :: (Int, Int) -> String -> String
 sliceOut (start, size) xs = take start xs <> drop (start + size) xs
@@ -54,11 +56,23 @@ filepathTests = testGroup "FilepathRemovalTests"
       "missing files: 'D:/x/y/z'"
       "missing files: ''"
 
-  , removeTest "division"
+  , removeTest "dollarRemoved"
+      "missing files: '/x/$y/z'"
+      "missing files: ''"
+
+  , removeTest "underscoreRemoved"
+      "missing files: '/x/_y/z'"
+      "missing files: ''"
+
+  , removeTest "hyphenRemoved"
+      "missing files: '/x/s-y/z'"
+      "missing files: ''"
+
+  , removeTest "divisionNotRemoved"
       "invalid operator / in"
       "invalid operator / in"
 
-  , removeTest "slash"
+  , removeTest "slashNotRemoved"
       "invalid operator \\ in"
       "invalid operator \\ in"
   ]
