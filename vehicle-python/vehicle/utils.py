0a7a91147bb069520bebfd5845d8586d62ff4714
Wen Kokke <wenkokke@users.noreply.github.com>
2022-11-21 15:38:15 +0000

    Support Python 3.8 (#275)

--- vehicle-python/vehicle/utils.py
+++ vehicle-python/vehicle/utils.py
@@ -1,4 +1,7 @@
-def internal_error_msg(msg):
+from typing import NoReturn
+
+
+def internal_error_msg(msg) -> NoReturn:
     raise Exception(
         "This is an internal error. Please raise an issue at https://github.com/vehicle-lang/vehicle/issues with the following error message: "
         + msg
