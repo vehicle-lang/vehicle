46021093d81d341e9d3647cc8f3ff329e9c5c417
nslusarz <nds1@hw.ac.uk>
2023-01-29 22:46:07 +0000

    removing a few files

--- vehicle-python/tests/utils.py
+++
@@ -1,8 +0,0 @@
-from typing import NoReturn
-
-
-def internal_error_msg(msg) -> NoReturn:
-    raise Exception(
-        "This is an internal error. Please raise an issue at https://github.com/vehicle-lang/vehicle/issues with the following error message: "
-        + msg
-    )
