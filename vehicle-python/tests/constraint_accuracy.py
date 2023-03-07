fc586f0a35c9c9ce87cf4ca52a65ebcd1937c874
nslusarz <nds1@hw.ac.uk>
2023-03-07 10:02:24 +0000

    optimising constraint acc

--- vehicle-python/tests/constraint_accuracy.py
+++ vehicle-python/tests/constraint_accuracy.py
@@ -9,12 +9,11 @@ model = keras.models.load_model('normal_training')
 
 def get_constraint_accuracy(model, X_data, y_data, epsilon, delta):
     counter = 0
-    for i in range(X_data.shape[0]):
-        x = X_data[i]
-        x_preturbed = x + np.random.uniform(-epsilon, epsilon, x.shape)
-        x = np.reshape(x, (-1, 28, 28))
-        a = np.linalg.norm(model.predict(x) - model.predict(x_preturbed))
-        if a < delta:
+    X_preturbed = X_data + np.random.uniform(-epsilon, epsilon, X_data.shape)
+    y_original = model.predict(X_data)
+    y_preturbed = model.predict(X_preturbed)
+    for i in range(y_original.shape[0]):
+        if np.linalg.norm(y_original[i] - y_preturbed[i]) < delta:
             counter+=1
     return counter/X_data.shape[0]
 
@@ -23,9 +22,6 @@ def get_constraint_accuracy(model, X_data, y_data, epsilon, delta):
 # Scale images to the [0, 1] range
 X_train = X_train.astype("float32") / 255
 X_test = X_test.astype("float32") / 255
-    # Make sure images have shape (28, 28, 1)
-    # X_train = np.expand_dims(X_train, -1)
-    # X_test = np.expand_dims(X_test, -1)
 
 y_train = keras.utils.to_categorical(y_train, 10)
 y_test = keras.utils.to_categorical(y_test, 10)
@@ -38,5 +34,6 @@ images = X_train[indexes]
 labels = y_train[indexes]
 model = keras.models.load_model('normal_training')
 
+#we use saved model 
 result = get_constraint_accuracy(model, images, labels, 0.01, 0.02)
 print(result)
