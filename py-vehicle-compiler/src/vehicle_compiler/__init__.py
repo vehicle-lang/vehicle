9ca615e4033c953a56aa4da9648f2224e3ab3d9d
Wen Kokke <wenkokke@users.noreply.github.com>
2023-04-03 19:58:52 +0100

    Fixes to *vehicle_core -> vehicle_compiler

--- py-vehicle-compiler/src/vehicle_compiler/__init__.py
+++ py-vehicle-compiler/src/vehicle_compiler/__init__.py
@@ -1,8 +1,8 @@
 from typing import List
 
-from vehicle_core._binding import hs_defaultMain as _hs_defaultMain
-from vehicle_core._binding import hs_rts_exit as _hs_rts_exit
-from vehicle_core._binding import hs_rts_init as _hs_rts_init
+from vehicle_compiler._binding import hs_defaultMain as _hs_defaultMain
+from vehicle_compiler._binding import hs_rts_exit as _hs_rts_exit
+from vehicle_compiler._binding import hs_rts_init as _hs_rts_init
 
 
 def main(args: List[str]):
--- py-vehicle-compiler/src/vehicle_compiler/__main__.py
+++ py-vehicle-compiler/src/vehicle_compiler/__main__.py
@@ -1,6 +1,6 @@
 import sys
 
-import vehicle_core
+import vehicle_compiler
 
 if __name__ == "__main__":
-    vehicle_core.main(sys.argv[1:])
+    vehicle_compiler.main(sys.argv[1:])
