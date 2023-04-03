a366d84ccf45b7c40272748d41f3ef487101144f
Wen Kokke <wenkokke@users.noreply.github.com>
2023-04-04 00:16:02 +0100

    Fix main function

--- py-vehicle-cli/src/vehicle_cli/__init__.py
+++ py-vehicle-cli/src/vehicle_cli/__init__.py
@@ -1,3 +1,4 @@
+import sys
 from typing import List, Optional
 
 from vehicle_cli._binding import hs_defaultMain as _hs_defaultMain
@@ -5,7 +6,11 @@ from vehicle_cli._binding import hs_rts_exit as _hs_rts_exit
 from vehicle_cli._binding import hs_rts_init as _hs_rts_init
 
 
-def main(args: Optional[List[str]] = None):
+def cli(args: Optional[List[str]] = None):
     _hs_rts_init(["vehicle"])
     _hs_defaultMain(args or [])
     _hs_rts_exit()
+
+
+def main():
+    cli(sys.argv[1:])
--- py-vehicle-cli/src/vehicle_cli/__main__.py
+++ py-vehicle-cli/src/vehicle_cli/__main__.py
@@ -1,6 +1,4 @@
-import sys
-
 import vehicle_cli
 
 if __name__ == "__main__":
-    vehicle_cli.main(sys.argv[1:])
+    vehicle_cli.main()
