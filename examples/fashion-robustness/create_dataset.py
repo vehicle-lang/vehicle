4d4c8a5e2a52e21c7690acef80a1e4a310bbb4bd
Tgl <marco1.casadio@mail.polimi.i>
2023-04-07 18:09:02 +0100

    fashion dataset

---
+++ examples/fashion-robustness/create_dataset.py
@@ -0,0 +1,13 @@
+from keras.datasets import fashion_mnist
+import idx2numpy
+
+
+(X_train, y_train), (X_test, y_test) = fashion_mnist.load_data()
+X_train = X_train / 255.0
+X_test = X_test /255.0
+
+idx2numpy.convert_to_file('examples/fashion-robustness/10k-images.idx', X_test)
+idx2numpy.convert_to_file('examples/fashion-robustness/10k-labels.idx', y_test)
+
+idx2numpy.convert_to_file('examples/fashion-robustness/1k-images.idx', X_test[:1000])
+idx2numpy.convert_to_file('examples/fashion-robustness/1k-labels.idx', y_test[:1000])
