05c208fb1d545b4dbb4debfa4e6b78d6d0c52622
Tgl <marco1.casadio@mail.polimi.i>
2023-04-11 09:41:48 +0100

    1 image test

--- examples/fashion-robustness/create_dataset.py
+++ examples/fashion-robustness/create_dataset.py
@@ -11,3 +11,6 @@ idx2numpy.convert_to_file('examples/fashion-robustness/10k-labels.idx', y_test)
 
 idx2numpy.convert_to_file('examples/fashion-robustness/1k-images.idx', X_test[:1000])
 idx2numpy.convert_to_file('examples/fashion-robustness/1k-labels.idx', y_test[:1000])
+
+idx2numpy.convert_to_file('examples/fashion-robustness/1-images.idx', X_test[:1])
+idx2numpy.convert_to_file('examples/fashion-robustness/1-labels.idx', y_test[:1])
