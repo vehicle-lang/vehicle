30b4ca42af5ae0524af573b1634d5f4a7b6dd273
Wen Kokke <wenkokke@users.noreply.github.com>
2023-04-18 13:17:19 +0100

    Rename primitives in Python binding

--- py-vehicle-lang/src/vehicle_lang/__init__.py
+++ py-vehicle-lang/src/vehicle_lang/__init__.py
@@ -1,25 +1,2 @@
-import sys
-from typing import List, Optional, Tuple
-
-from vehicle_lang._internal import unsafeVehicleMain
-from vehicle_lang._tempfiles import tempfiles
+from vehicle_lang._internal import vehicleSync as vehicleSync
 from vehicle_lang._version import VERSION as VERSION
-
-
-def vehicleSync(
-    args: Optional[List[str]] = None,
-) -> Tuple[int, Optional[str], Optional[str], Optional[str]]:
-    with tempfiles("out", "err", "log", prefix="vehicle") as (out, err, log):
-        exc = unsafeVehicleMain(
-            [
-                f"--redirect-stdout={out}",
-                f"--redirect-stderr={err}",
-                f"--redirect-logs={log}",
-                *(args or []),
-            ]
-        )
-        return (exc, out.read_text(), err.read_text(), log.read_text())
-
-
-def main():
-    exit(unsafeVehicleMain(sys.argv[1:]))
