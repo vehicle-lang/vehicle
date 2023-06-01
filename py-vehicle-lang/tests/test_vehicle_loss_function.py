e22725b2fe04bdf4c438e516f132e582a716c0cd
Wen Kokke <wenkokke@users.noreply.github.com>
2023-06-01 15:44:39 +0100

    Parameterize test_loss_function_json

--- py-vehicle-lang/tests/test_vehicle_loss_function.py
+++ py-vehicle-lang/tests/test_vehicle_loss_function.py
@@ -2,16 +2,20 @@ import json
 from pathlib import Path
 from typing import List
 
+import pytest
 
-def test_loss_function_json() -> None:
+GOLDEN_PATH = Path("vendor") / "vehicle" / "tests" / "golden" / "compile"
+
+
+@pytest.mark.parametrize(
+    "loss_function_json", GOLDEN_PATH.glob("**/LossFunction.json.golden")
+)  # type: ignore[misc]
+def test_loss_function_json(loss_function_json: Path) -> None:
     from vehicle_lang.loss_function import Declaration
 
-    golden_dir = Path("vendor") / "vehicle" / "tests" / "golden" / "compile"
-    for golden_file in golden_dir.glob("**/LossFunction.json.golden"):
-        print(f"Test {golden_file}")
-        value = json.loads(golden_file.read_text())
-        if not isinstance(value, List):
-            value = [value]
-        for item in value:
-            declaration = Declaration.from_dict(item)
-            assert item == json.loads(declaration.to_json())
+    value = json.loads(loss_function_json.read_text())
+    if not isinstance(value, List):
+        value = [value]
+    for item in value:
+        declaration = Declaration.from_dict(item)
+        assert item == json.loads(declaration.to_json())
