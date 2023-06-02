54583040354c198a2c54212fe595c0768edcfd25
Wen Kokke <wenkokke@users.noreply.github.com>
2023-06-02 19:56:55 +0100

    Make _decode modular

--- py-vehicle-lang/src/vehicle_lang/loss_function/_decode.py
+++ py-vehicle-lang/src/vehicle_lang/loss_function/_decode.py
@@ -1,8 +1,11 @@
 import collections.abc
-from dataclasses import MISSING, dataclass, fields, is_dataclass
+from abc import ABCMeta, abstractmethod
+from dataclasses import MISSING, dataclass, field, fields, is_dataclass
 from typing import (
     Any,
+    Callable,
     Dict,
+    Generic,
     Iterator,
     List,
     Optional,
@@ -13,8 +16,17 @@ from typing import (
     cast,
 )
 
-from typing_extensions import Literal, TypeAlias, TypeVar, get_args, get_origin
+from typing_extensions import (
+    Literal,
+    TypeAlias,
+    TypeVar,
+    assert_never,
+    get_args,
+    get_origin,
+    override,
+)
 
+_S = TypeVar("_S")
 _T = TypeVar("_T")
 
 
@@ -22,181 +34,88 @@ JsonValue: TypeAlias = Union[
     str, bool, int, float, complex, List["JsonValue"], Dict[str, "JsonValue"]
 ]
 
-_TYPE: str = "tag"
-_ARGS: str = "contents"
-
-
-def _type_name(cls: Any) -> str:
-    if isinstance(cls, type):
-        return cls.__name__
-    else:
-        return str(cls)
-
-
-def _subcls_iter(cls: Type[_T]) -> Iterator[Type[_T]]:
-    """
-    Iterate over a class and its subclasses.
-    """
-    yield cls
-    yield from cls.__subclasses__()
-
-
-def _subcls_find(cls: Type[_T], cls_name: str) -> Optional[Type[_T]]:
-    """
-    Find a subclass by name.
-    """
-    for subcls in _subcls_iter(cls):
-        if subcls.__name__ == cls_name:
-            return subcls
-    return None
-
 
 @dataclass
 class DecodeError(Exception):
     value: JsonValue
     cls: Any
     reason: Optional[str] = None
+    telescope: Sequence[Tuple[Type[Any], str]] = ()
 
     def __str__(self) -> str:
         expected_type_str = _type_name(self.cls)
         found_type_str = _type_name(type(self.value))
+        telescope_str = " > ".join(
+            [f"{_type_name(cls)}.{fld}" for cls, fld in self.telescope]
+        )
         return "".join(
             [
                 f"Could not decode value of type {found_type_str} as {expected_type_str}",
                 f": {self.reason}." if self.reason else ".",
+                f" ({telescope_str})" if self.telescope else "",
                 f"\n{repr(self.value)}",
             ]
         )
 
 
-@dataclass
-class DecodeFieldError(Exception):
-    value: JsonValue
-    telescope: Sequence[Tuple[Type[Any], str]]
-    reason: DecodeError
+class Decoder(Generic[_T], metaclass=ABCMeta):
+    @abstractmethod
+    def decode(
+        self,
+        decoder: "JsonDecoder",
+        cls_origin: Any,
+        cls_args: Tuple[Any, ...],
+        value: JsonValue,
+    ) -> _T:
+        ...
 
-    def __str__(self) -> str:
-        telescope_str = " > ".join(
-            [f"{_type_name(cls)}.{fld}" for cls, fld in self.telescope]
-        )
-        return f"Could not decode {telescope_str}: {self.reason}"
 
+class TaggedObjectDecoder(Decoder[_T]):
+    TAG: str = "tag"
+    CONTENTS: str = "contents"
 
-def decode(cls: Union[Type[_T], Any], value: JsonValue) -> _T:
-    # str
-    if cls is str:
-        if not isinstance(value, str):
-            raise DecodeError(value, cls)
-        return cast(_T, value)
+    @staticmethod
+    def _find_class(cls: Type[_T], cls_name: str) -> Optional[Type[_T]]:
+        """
+        Find a subclass by name.
+        """
+        for subcls in TaggedObjectDecoder._class_and_subclasses(cls):
+            if subcls.__name__ == cls_name:
+                return subcls
+        return None
 
-    # int
-    if cls is int:
-        if not isinstance(value, int):
-            raise DecodeError(value, cls)
-        return cast(_T, value)
+    @staticmethod
+    def _class_and_subclasses(cls: Type[_T]) -> Iterator[Type[_T]]:
+        """
+        Iterate over a class and its subclasses.
+        """
+        yield cls
+        yield from cls.__subclasses__()
 
-    # float
-    if cls is float:
-        if not isinstance(value, (int, float)):
-            raise DecodeError(value, cls)
-        return cast(_T, float(value))
+    @override
+    def decode(
+        self,
+        decoder: "JsonDecoder",
+        cls_origin: Type[_T],
+        cls_args: Tuple[Any, ...],
+        value: JsonValue,
+    ) -> _T:
+        if not is_dataclass(cls_origin):
+            raise DecodeError(value, cls_origin, "not a dataclass")
 
-    # complex
-    if cls is complex:
-        if not isinstance(value, (int, float, complex)):
-            raise DecodeError(value, cls)
-        return cast(_T, complex(value))
-
-    # tuple
-    if get_origin(cls) in (tuple,):
-        if not isinstance(value, List):
-            raise DecodeError(value, cls)
-        cls_args = get_args(cls)
-
-        # tuple[()]
-        if len(cls_args) == 1 and cls_args[0] is ():
-            if not value:
-                return cast(_T, ())
-            else:
-                raise DecodeError(value, cls, "mismatched length")
-
-        # tuple[_T, ...]
-        if len(cls_args) == 2 and cls_args[1] is ...:
-            cls_item = cls_args[0]
-            return cast(_T, [decode(cls_item, item) for item in value])
-
-        # tuple
-        if len(cls_args) != len(value):
-            raise DecodeError(value, cls, "mismatched length")
-        return cast(
-            _T,
-            tuple((decode(cls_item, item) for cls_item, item in zip(cls_args, value))),
-        )
-
-    # list, Sequence, MutableSequence
-    if get_origin(cls) in (
-        list,
-        collections.abc.Sequence,
-        collections.abc.MutableSequence,
-    ):
-        if not isinstance(value, List):
-            raise DecodeError(value, cls)
-        try:
-            cls_item, *_ = get_args(cls)
-        except ValueError:
-            raise TypeError(f"{cls.__name__} requires a type annotation")
-        return cast(_T, [decode(cls_item, item) for item in value])
-
-    # dict, Mapping, MutableMapping
-    if get_origin(cls) in (
-        dict,
-        collections.abc.Mapping,
-        collections.abc.MutableMapping,
-    ):
         if not isinstance(value, Dict):
-            raise DecodeError(value, cls)
-        try:
-            cls_key, cls_val, *_ = get_args(cls)
-        except ValueError:
-            raise TypeError(f"{cls.__name__} requires a type annotation")
-        return cast(
-            _T,
-            {decode(cls_key, key): decode(cls_val, val) for key, val in value.items()},
-        )
-
-    # Literal
-    if get_origin(cls) is Literal:
-        cls_args = get_args(cls)
-        if value in cls_args:
-            return cast(_T, value)
-        else:
-            reason = f"expected value to be one of {', '.join(_type_name(cls_arg) for cls_arg in cls_args)}"
-            raise DecodeError(value, cls, reason)
-
-    # Union
-    if get_origin(cls) is Union:
-        cls_args = get_args(cls)
-        for cls_alt in cls_args:
-            try:
-                return decode(cls_alt, value)
-            except DecodeError:
-                continue
-        reason = f"expected value to be of type {' | '.join(_type_name(cls_arg) for cls_arg in cls_args)}"
-        raise DecodeError(value, cls, reason)
-
-    # Dataclass from {_TAG: "<cls>", _CONTENTS: [...]}
-    if is_dataclass(cls):
-        if not isinstance(value, Dict):
-            raise DecodeError(value, cls, "expected dict")
+            raise DecodeError(value, cls_origin, "expected dict")
 
         # Find the subclass
-        if _TYPE not in value:
-            raise DecodeError(value, cls, f"missing field '{_TYPE}'")
+        if self.TAG not in value:
+            raise DecodeError(value, cls_origin, f"missing field '{self.TAG}'")
 
-        subcls_name: str = decode(str, value[_TYPE])
-        subcls: Optional[Type[_T]] = _subcls_find(cast(Type[_T], cls), subcls_name)
+        subcls_name: str = decoder.decode(str, value[self.TAG])
+        subcls: Optional[Type[_T]] = TaggedObjectDecoder._find_class(
+            cast(Type[_T], cls_origin), subcls_name
+        )
         if subcls is None:
-            raise DecodeError(value, cls, f"could not find class {subcls_name}")
+            raise DecodeError(value, cls_origin, f"could not find class {subcls_name}")
         if not is_dataclass(subcls):
             raise DecodeError(value, subcls, f"not a dataclass")
 
@@ -213,23 +132,23 @@ def decode(cls: Union[Type[_T], Any], value: JsonValue) -> _T:
             and (fld.default is not MISSING or fld.default_factory is not MISSING)
         ]
         if len(required_init_fields) == 0:
-            if _ARGS in value and value[_ARGS]:
-                raise DecodeError(value, subcls, f"unused field '{_ARGS}'")
+            if self.CONTENTS in value and value[self.CONTENTS]:
+                raise DecodeError(value, subcls, f"unused field '{self.CONTENTS}'")
             else:
                 return cast(_T, subcls())
 
         # Get the arguments field:
-        if _ARGS not in value:
-            raise DecodeError(value, subcls, f"missing field '{_ARGS}'")
-        value_args = value[_ARGS]
+        if self.CONTENTS not in value:
+            raise DecodeError(value, subcls, f"missing field '{self.CONTENTS}'")
+        value_args = value[self.CONTENTS]
 
         # Special case: If there is only one required argument
         if len(required_init_fields) == 1:
             try:
                 fld = required_init_fields[0]
-                arg: Any = decode(fld.type, value_args)
+                arg: Any = decoder.decode(fld.type, value_args)
                 return cast(_T, subcls(*[arg]))
-            except (DecodeError, DecodeFieldError) as e:
+            except DecodeError as e:
                 value_args = [value_args]
 
         # Decode the arguments:
@@ -240,18 +159,230 @@ def decode(cls: Union[Type[_T], Any], value: JsonValue) -> _T:
         for index, fld in enumerate(required_init_fields):
             if index < len(value_args):
                 try:
-                    args.append(decode(fld.type, value_args[index]))
+                    args.append(decoder.decode(fld.type, value_args[index]))
                 except DecodeError as e:
-                    raise DecodeFieldError(value, ((subcls, fld.name),), e)
-                except DecodeFieldError as e:
-                    raise DecodeFieldError(
-                        value, ((subcls, fld.name), *e.telescope), e.reason
+                    raise DecodeError(
+                        value,
+                        subcls,
+                        e.reason,
+                        telescope=((subcls, fld.name), *e.telescope),
                     )
             else:
                 raise DecodeError(value, subcls, f"missing value for {fld.name}")
 
-        # TODO: parse optional arguments
-
         return cast(_T, subcls(*args))
 
-    raise DecodeError(value, cls)
+
+def _decode_str(value: JsonValue) -> str:
+    if not isinstance(value, str):
+        raise DecodeError(value, str, "expected str")
+    return value
+
+
+def _decode_int(value: JsonValue) -> int:
+    if not isinstance(value, int):
+        raise DecodeError(value, int, "expected int")
+    return value
+
+
+def _decode_float(value: JsonValue) -> float:
+    if not isinstance(value, (int, float)):
+        raise DecodeError(value, float, "expected int or float")
+    return value
+
+
+def _decode_complex(value: JsonValue) -> complex:
+    if not isinstance(value, (int, float, complex)):
+        raise DecodeError(value, complex, "expected int, float, or complex")
+    return value
+
+
+class TupleDecoder(Decoder[Any]):
+    @override
+    def decode(
+        self,
+        decoder: "JsonDecoder",
+        cls_origin: Any,
+        cls_args: Tuple[Any, ...],
+        value: JsonValue,
+    ) -> Any:
+        if cls_origin is not tuple:
+            raise DecodeError(value, cls_origin, "expected tuple")
+
+        if not isinstance(value, List):
+            raise DecodeError(value, cls_origin, "expected list")
+
+        # tuple[()]
+        if len(cls_args) == 1 and cls_args[0] is ():
+            if not value:
+                return ()
+            else:
+                raise DecodeError(value, cls_origin, "mismatched length")
+
+        # tuple[_T, ...]
+        if len(cls_args) == 2 and cls_args[1] is ...:
+            return tuple(*(decoder.decode(cls_args[0], item) for item in value))
+
+        # tuple[_P]
+        if len(cls_args) != len(value):
+            raise DecodeError(value, cls_origin, "mismatched length")
+
+        return tuple(
+            *(decoder.decode(cls_item, item) for cls_item, item in zip(cls_args, value))
+        )
+
+
+class ListDecoder(Decoder[List[_T]]):
+    @override
+    def decode(
+        self,
+        decoder: "JsonDecoder",
+        cls_origin: Type[List[_T]],
+        cls_args: Tuple[Any, ...],
+        value: JsonValue,
+    ) -> List[_T]:
+        if not isinstance(value, List):
+            raise DecodeError(value, cls_origin, "expected list")
+
+        if len(cls_args) <= 0:
+            raise DecodeError(value, cls_origin, f"list type requires an argument")
+
+        if len(cls_args) >= 2:
+            raise DecodeError(
+                value, cls_origin, f"too many arguments: {', '.join(cls_args)}"
+            )
+
+        cls_item = cls_args[0]
+        return [decoder.decode(cls_item, item) for item in value]
+
+
+class DictDecoder(Decoder[Dict[_S, _T]]):
+    @override
+    def decode(
+        self,
+        decoder: "JsonDecoder",
+        cls_origin: Type[Dict[_S, _T]],
+        cls_args: Tuple[Any, ...],
+        value: JsonValue,
+    ) -> Dict[_S, _T]:
+        if not isinstance(value, Dict):
+            raise DecodeError(value, cls_origin, "expected dict")
+
+        if len(cls_args) <= 1:
+            raise DecodeError(value, cls_origin, f"dict type requires arguments")
+
+        if len(cls_args) >= 3:
+            raise DecodeError(
+                value, cls_origin, f"too many arguments: {', '.join(cls_args)}"
+            )
+
+        cls_S = cls_args[0]
+        cls_T = cls_args[1]
+
+        return {
+            decoder.decode(cls_S, key): decoder.decode(cls_T, val)
+            for key, val in value.items()
+        }
+
+
+class LiteralDecoder(Decoder[Any]):
+    @override
+    def decode(
+        self,
+        decoder: "JsonDecoder",
+        cls_origin: Any,
+        cls_args: Tuple[Any, ...],
+        value: JsonValue,
+    ) -> Any:
+        if value in cls_args:
+            return value
+        else:
+            reason = f"expected value to be one of {', '.join(_type_name(cls_arg) for cls_arg in cls_args)}"
+            raise DecodeError(value, cls_origin, reason)
+
+
+class UnionDecoder(Decoder[Any]):
+    @override
+    def decode(
+        self,
+        decoder: "JsonDecoder",
+        cls_origin: Any,
+        cls_args: Tuple[Any, ...],
+        value: JsonValue,
+    ) -> Any:
+        for cls_arg in cls_args:
+            try:
+                return decoder.decode(cls_arg, value)
+            except DecodeError:
+                continue
+        reason = f"expected value to be of type {' | '.join(_type_name(cls_arg) for cls_arg in cls_args)}"
+        raise DecodeError(value, cls_origin, reason)
+
+
+AnyDecoder: TypeAlias = Union[Decoder[Any], Callable[[JsonValue], Any]]
+
+
+@dataclass(frozen=True)
+class JsonDecoder:
+    dataclass_decoder: AnyDecoder
+    decoders: Dict[Any, AnyDecoder] = field(init=False, default_factory=dict)
+
+    def register(
+        self,
+        cls: Union[Type[_T], Any],
+        decoder: AnyDecoder,
+    ) -> None:
+        cls_origin = get_origin(cls) or cls
+        self.decoders[cls_origin] = decoder
+
+    def decode(
+        self,
+        cls: Union[Type[_T], Any],
+        value: JsonValue,
+    ) -> _T:
+        cls_origin = get_origin(cls) or cls
+        cls_args = get_args(cls)
+        if is_dataclass(cls_origin):
+            if isinstance(self.dataclass_decoder, Decoder):
+                return cast(
+                    _T, self.dataclass_decoder.decode(self, cls_origin, cls_args, value)
+                )
+            elif callable(self.dataclass_decoder):
+                return cast(_T, self.dataclass_decoder(value))
+            assert_never()
+        else:
+            decoder = self.decoders.get(cls_origin)
+            if decoder is None:
+                raise DecodeError(value, cls, f"no decoder for {_type_name(cls)}")
+            elif isinstance(decoder, Decoder):
+                return cast(_T, decoder.decode(self, cls_origin, cls_args, value))
+            elif callable(decoder):
+                return cast(_T, decoder(value))
+            assert_never()
+
+
+_DEFAULT_DECODER: JsonDecoder = JsonDecoder(dataclass_decoder=TaggedObjectDecoder())
+_DEFAULT_DECODER.register(str, _decode_str)
+_DEFAULT_DECODER.register(int, _decode_int)
+_DEFAULT_DECODER.register(float, _decode_float)
+_DEFAULT_DECODER.register(complex, _decode_complex)
+_DEFAULT_DECODER.register(tuple, TupleDecoder())
+_DEFAULT_DECODER.register(list, ListDecoder())
+_DEFAULT_DECODER.register(collections.abc.Sequence, ListDecoder())
+_DEFAULT_DECODER.register(collections.abc.MutableSequence, ListDecoder())
+_DEFAULT_DECODER.register(dict, DictDecoder())
+_DEFAULT_DECODER.register(collections.abc.Mapping, DictDecoder())
+_DEFAULT_DECODER.register(collections.abc.MutableMapping, DictDecoder())
+_DEFAULT_DECODER.register(Literal, LiteralDecoder())
+_DEFAULT_DECODER.register(Union, UnionDecoder())
+
+
+def decode(cls: Union[Type[_T], Any], value: JsonValue) -> _T:
+    return _DEFAULT_DECODER.decode(cls, value)
+
+
+def _type_name(cls: Any) -> str:
+    if isinstance(cls, type):
+        return cls.__name__
+    else:
+        return str(cls)
