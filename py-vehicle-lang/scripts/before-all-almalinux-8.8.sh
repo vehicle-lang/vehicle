d33169a6963f594304d3964fe5ca5a909f6d2d1b
Wen Kokke <wenkokke@users.noreply.github.com>
2023-06-17 23:13:49 +0100

    Configure tox to work on linux (#577)
    * Configure tox to work on linux
    
    * Add build-wheel-manylinux
    
    * Add version to before-all; Build for aarch64 linux

--- py-vehicle-lang/scripts/before-all-almalinux-8.8.sh
+++ py-vehicle-lang/scripts/before-all-almalinux-8.8.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-# NOTE: The CentOS build of GHC links against "libgmp.so"
+# NOTE: The Debian 10 build of GHC links against "libgmp.so"
 cd /usr/lib64 && ln -s libgmp.so.10 libgmp.so
 
 # Install prerequisites
 yum install -y wget
 
 # GHC release URL
-GHC_RELEASE_URL="https://downloads.haskell.org/~ghc/9.4.4/ghc-9.4.4-x86_64-centos7-linux.tar.xz"
+GHC_RELEASE_URL="https://downloads.haskell.org/~ghc/9.4.4/ghc-9.4.4-aarch64-deb10-linux.tar.xz"
 
 # Install GHC
 wget -q "${GHC_RELEASE_URL}" -O /tmp/ghc.tar.xz
@@ -20,9 +20,8 @@ cd /tmp/ghc && make install
 CABAL_RELEASE_URL="https://github.com/haskell/cabal/archive/refs/tags/cabal-install-v3.10.1.0.zip"
 
 # Install Cabal
-wget "${CABAL_RELEASE_URL}" -O /tmp/cabal.zip
+wget -q "${CABAL_RELEASE_URL}" -O /tmp/cabal.zip
 unzip -q /tmp/cabal.zip -d /tmp && mv /tmp/cabal-cabal-install-v3.10.1.0 /tmp/cabal
-sed -ie 's/+ofd-locking/-ofd-locking/' /tmp/cabal/bootstrap/linux-9.4.4.json
-cd /tmp/cabal && python3 ./bootstrap/bootstrap.py -d ./bootstrap/linux-9.4.4.json -w /usr/local/bin/ghc-9.4.4
+cd /tmp/cabal && python3.11 ./bootstrap/bootstrap.py -d ./bootstrap/linux-9.4.4.json -w /usr/local/bin/ghc-9.4.4
 cd /tmp/cabal/cabal-install && /tmp/cabal/_build/bin/cabal v2-update
-cd /tmp/cabal/cabal-install && /tmp/cabal/_build/bin/cabal v2-install cabal-install --constraint='lukko -ofd-locking' --overwrite-policy=always --install-method=copy --installdir=/usr/local/bin
+cd /tmp/cabal/cabal-install && /tmp/cabal/_build/bin/cabal v2-install cabal-install --overwrite-policy=always --install-method=copy --installdir=/usr/local/bin
---
+++ py-vehicle-lang/scripts/build-wheel-manylinux2014_x86_64.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+manylinux_image="quay.io/pypa/manylinux2014_x86_64"
+
+# See: https://stackoverflow.com/a/4774063
+package_root="$(dirname "$(dirname "$( cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit ; pwd -P )" )" )"
+
+# Check if 'docker' is on the PATH:
+docker=$(which docker || echo > "$fail")
+if [ -s "$fail" ]; then
+    rm "$fail"
+    echo "${0} requires 'docker'."
+    echo "See: https://docs.docker.com/get-docker/"
+    exit 1
+fi
+
+# Create a container for the build
+"${docker}" run \
+  --rm \
+  --volume="${package_root}:/vehicle" \
+  --workdir="/vehicle/py-vehicle-lang" "${manylinux_image}" \
+  /bin/sh -c "sh ./scripts/before-all-centos-7.sh && pipx run tox"
