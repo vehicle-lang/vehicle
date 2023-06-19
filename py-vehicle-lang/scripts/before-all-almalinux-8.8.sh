34c595f0015e5ee3b9701f06dad7b3ff5c410935
Wen Kokke <wenkokke@users.noreply.github.com>
2023-06-19 10:10:08 +0100

    Remove alpine & fix centos and almalinux

--- py-vehicle-lang/scripts/before-all-almalinux-8.8.sh
+++ py-vehicle-lang/scripts/before-all-almalinux-8.8.sh
@@ -1,27 +1,43 @@
 #!/bin/sh
 
-# NOTE: The Debian 10 build of GHC links against "libgmp.so"
-cd /usr/lib64 && ln -s libgmp.so.10 libgmp.so
-
 # Install prerequisites
-yum install -y wget
+python3.11 -m pip install wget
+
+# Determine architecture
+machine="$(python3.11 -c 'import platform; print(platform.machine())')"
+
+# Link libgmp.so.10 to libgmp.so
+if [ "${machine}" = 'x86_64' ]; then
+  cd "/usr/lib64" && ln -s "libgmp.so.10" "libgmp.so"
+fi
+
+# GHC version
+GHC_VERSION="9.4.4"
 
 # GHC release URL
-GHC_RELEASE_URL="https://downloads.haskell.org/~ghc/9.4.4/ghc-9.4.4-aarch64-deb10-linux.tar.xz"
+if [ "${machine}" = 'aarch64' ]; then
+  GHC_RELEASE_URL="https://downloads.haskell.org/~ghc/${GHC_VERSION}/ghc-${GHC_VERSION}-aarch64-deb10-linux.tar.xz"
+elif [ "${machine}" = 'x86_64' ]; then
+  GHC_RELEASE_URL="https://downloads.haskell.org/~ghc/${GHC_VERSION}/ghc-${GHC_VERSION}-x86_64-deb10-linux.tar.xz"
+else
+  echo "unsupported: ${machine}"
+  exit 1
+fi
 
 # Install GHC
-wget -q "${GHC_RELEASE_URL}" -O /tmp/ghc.tar.xz
-mkdir /tmp/ghc
-tar xf /tmp/ghc.tar.xz -C /tmp/ghc --strip-components 1
-cd /tmp/ghc && ./configure
-cd /tmp/ghc && make install
+python3.11 -c "import wget; wget.download('${GHC_RELEASE_URL}', '/tmp/ghc.tar.xz')"
+mkdir "/tmp/ghc"
+tar xf "/tmp/ghc.tar.xz" -C "/tmp/ghc" --strip-components 1
+cd "/tmp/ghc" && ./configure
+cd "/tmp/ghc" && make install
 
-# Cabal release URL
-CABAL_RELEASE_URL="https://github.com/haskell/cabal/archive/refs/tags/cabal-install-v3.10.1.0.zip"
+# Cabal version and release URL
+CABAL_VERSION="3.10.1.0"
+CABAL_RELEASE_URL="https://github.com/haskell/cabal/archive/refs/tags/cabal-install-v${CABAL_VERSION}.zip"
 
 # Install Cabal
-wget -q "${CABAL_RELEASE_URL}" -O /tmp/cabal.zip
-unzip -q /tmp/cabal.zip -d /tmp && mv /tmp/cabal-cabal-install-v3.10.1.0 /tmp/cabal
-cd /tmp/cabal && python3.11 ./bootstrap/bootstrap.py -d ./bootstrap/linux-9.4.4.json -w /usr/local/bin/ghc-9.4.4
-cd /tmp/cabal/cabal-install && /tmp/cabal/_build/bin/cabal v2-update
-cd /tmp/cabal/cabal-install && /tmp/cabal/_build/bin/cabal v2-install cabal-install --overwrite-policy=always --install-method=copy --installdir=/usr/local/bin
+python3.11 -c "import wget; wget.download('${CABAL_RELEASE_URL}', '/tmp/cabal.zip')"
+unzip -q "/tmp/cabal.zip" -d "/tmp" && mv "/tmp/cabal-cabal-install-v${CABAL_VERSION}" "/tmp/cabal"
+cd "/tmp/cabal" && python3.11 "./bootstrap/bootstrap.py" -d "./bootstrap/linux-${GHC_VERSION}.json" -w "/usr/local/bin/ghc-${GHC_VERSION}"
+cd "/tmp/cabal/cabal-install" && /tmp/cabal/_build/bin/cabal v2-update
+cd "/tmp/cabal/cabal-install" && /tmp/cabal/_build/bin/cabal v2-install cabal-install --overwrite-policy=always --install-method=copy --installdir="/usr/local/bin"
--- py-vehicle-lang/scripts/before-all-centos-7.sh
+++ py-vehicle-lang/scripts/before-all-centos-7.sh
@@ -1,28 +1,46 @@
 #!/bin/sh
 
-# NOTE: The CentOS build of GHC links against "libgmp.so"
-cd /usr/lib64 && ln -s libgmp.so.10 libgmp.so
-
 # Install prerequisites
-yum install -y wget
+python3 -m pip install wget
+
+# Determine architecture
+machine="$(python3 -c 'import platform; print(platform.machine())')"
+
+# Link libgmp.so.10 to libgmp.so
+if [ "${machine}" = 'i386' ] || [ "${machine}" = 'i686' ]; then
+  cd "/usr/lib" && ln -s "libgmp.so.10" "libgmp.so"
+elif [ "${machine}" = 'x86_64' ]; then
+  cd "/usr/lib64" && ln -s "libgmp.so.10" "libgmp.so"
+fi
+
+# GHC version
+GHC_VERSION="9.4.4"
 
 # GHC release URL
-GHC_RELEASE_URL="https://downloads.haskell.org/~ghc/9.4.4/ghc-9.4.4-x86_64-centos7-linux.tar.xz"
+if [ "${machine}" = 'i386' ] || [ "${machine}" = 'i686' ]; then
+  GHC_RELEASE_URL="https://downloads.haskell.org/~ghc/${GHC_VERSION}/ghc-${GHC_VERSION}-i386-deb9-linux.tar.xz"
+elif [ "${machine}" = 'x86_64' ]; then
+  GHC_RELEASE_URL="https://downloads.haskell.org/~ghc/${GHC_VERSION}/ghc-${GHC_VERSION}-x86_64-centos7-linux.tar.xz"
+else
+  echo "unsupported: ${machine}"
+  exit 1
+fi
 
 # Install GHC
-wget -q "${GHC_RELEASE_URL}" -O /tmp/ghc.tar.xz
-mkdir /tmp/ghc
-tar xf /tmp/ghc.tar.xz -C /tmp/ghc --strip-components 1
-cd /tmp/ghc && ./configure
-cd /tmp/ghc && make install
+python3 -c "import wget; wget.download('${GHC_RELEASE_URL}', '/tmp/ghc.tar.xz')"
+mkdir "/tmp/ghc"
+tar xf "/tmp/ghc.tar.xz" -C "/tmp/ghc" --strip-components 1
+cd "/tmp/ghc" && ./configure
+cd "/tmp/ghc" && make install
 
-# Cabal release URL
-CABAL_RELEASE_URL="https://github.com/haskell/cabal/archive/refs/tags/cabal-install-v3.10.1.0.zip"
+# Cabal version and release URL
+CABAL_VERSION="3.10.1.0"
+CABAL_RELEASE_URL="https://github.com/haskell/cabal/archive/refs/tags/cabal-install-v${CABAL_VERSION}.zip"
 
 # Install Cabal
-wget "${CABAL_RELEASE_URL}" -O /tmp/cabal.zip
-unzip -q /tmp/cabal.zip -d /tmp && mv /tmp/cabal-cabal-install-v3.10.1.0 /tmp/cabal
-sed -ie 's/+ofd-locking/-ofd-locking/' /tmp/cabal/bootstrap/linux-9.4.4.json
-cd /tmp/cabal && python3.11 ./bootstrap/bootstrap.py -d ./bootstrap/linux-9.4.4.json -w /usr/local/bin/ghc-9.4.4
-cd /tmp/cabal/cabal-install && /tmp/cabal/_build/bin/cabal v2-update
-cd /tmp/cabal/cabal-install && /tmp/cabal/_build/bin/cabal v2-install cabal-install --constraint='lukko -ofd-locking' --overwrite-policy=always --install-method=copy --installdir=/usr/local/bin
+python3 -c "import wget; wget.download('${CABAL_RELEASE_URL}', '/tmp/cabal.zip')"
+unzip -q "/tmp/cabal.zip" -d "/tmp" && mv "/tmp/cabal-cabal-install-v${CABAL_VERSION}" "/tmp/cabal"
+sed -ie 's/+ofd-locking/-ofd-locking/' "/tmp/cabal/bootstrap/linux-${GHC_VERSION}.json"
+cd "/tmp/cabal" && python3 "./bootstrap/bootstrap.py" -d "./bootstrap/linux-${GHC_VERSION}.json" -w "/usr/local/bin/ghc-${GHC_VERSION}"
+cd "/tmp/cabal/cabal-install" && /tmp/cabal/_build/bin/cabal v2-update
+cd "/tmp/cabal/cabal-install" && /tmp/cabal/_build/bin/cabal v2-install cabal-install --constraint='lukko -ofd-locking' --overwrite-policy=always --install-method=copy --installdir="/usr/local/bin"
