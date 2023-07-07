bbeb0dff203b8b0a83f2e74e2d50ca7d0893cc1c
Wen Kokke <wenkokke@users.noreply.github.com>
2023-07-07 13:11:07 +0100

    Add uncaught exception handler

--- vehicle-python/src/VehiclePythonBinding.hs
+++ vehicle-python/src/VehiclePythonBinding.hs
@@ -3,18 +3,26 @@
 
 module VehiclePythonBinding where
 
+import Control.Exception (Exception (..), SomeException (..), handle)
 import Foreign.C.String (CString, peekCString)
 import Foreign.C.Types (CInt (..))
 import Foreign.Marshal.Array (peekArray)
 import Foreign.Ptr (Ptr)
+import System.IO (hPutStrLn, stderr)
 import Vehicle (mainWithArgsAndExitCode)
 
+uncaughtException :: SomeException -> IO CInt
+uncaughtException (SomeException e) = do
+  hPutStrLn stderr (displayException e)
+  return 1
+
 _unsafe_vehicle_main :: CInt -> Ptr CString -> IO CInt
-_unsafe_vehicle_main argc argv = do
-  -- Convert from `Ptr CString` to `[String]`
-  args <- mapM peekCString =<< peekArray (fromIntegral argc) argv
-  -- Call Vehicle.main and return the exit code
-  exitCode <- mainWithArgsAndExitCode args
-  return (fromIntegral exitCode)
+_unsafe_vehicle_main argc argv =
+  handle uncaughtException $ do
+    -- Convert from `Ptr CString` to `[String]`
+    args <- mapM peekCString =<< peekArray (fromIntegral argc) argv
+    -- Call Vehicle.main and return the exit code
+    exitCode <- mainWithArgsAndExitCode args
+    return (fromIntegral exitCode)
 
 foreign export ccall _unsafe_vehicle_main :: CInt -> Ptr CString -> IO CInt
