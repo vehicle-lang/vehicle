19ae0e886a51746e5c7003f6df069ad7228e958b
Wen Kokke <wenkokke@users.noreply.github.com>
2023-08-01 21:11:42 +0100

    Minor changes

--- docs/conf.py
+++ docs/conf.py
@@ -1,10 +1,15 @@
 # Configuration file for the Sphinx documentation builder.
 
+import pathlib
+import sys
+
+sys.path.insert(0, pathlib.Path(__file__).parent / "vehicle-python" / "src")
+
 # -- Project information
 
 project = "Vehicle"
 copyright = "2022"
-author = "Matthew Daggitt, Wen Kokke et al."
+author = "Matthew Daggitt, Wen Kokke, et al."
 
 release = "0.1"
 version = "0.1.0"
@@ -36,12 +41,20 @@ extlinks = {".": ("https://github.com/vehicle-lang/vehicle/tree/dev/%s", "./%s")
 
 exclude_patterns = [".tox"]
 
+# -- Options for autosummary
+
+autosummary_mock_imports = ["vehicle_lang._binding"]
+
+autosummary_imported_members = True
+
+autosummary_ignore_module_all = False
+
 # -- Options for MyST --------------------------------------------------------
 
 myst_enable_extensions = [
     # Enables colon fence directives
     # https://myst-parser.readthedocs.io/en/latest/syntax/optional.html#syntax-colon-fence
-    "colon_fence",
+    # "colon_fence",
     # Enables definition lists
     # https://myst-parser.readthedocs.io/en/latest/syntax/optional.html#definition-lists
     "deflist",
