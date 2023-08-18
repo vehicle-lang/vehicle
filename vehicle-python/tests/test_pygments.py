83353f5a3c52507b0f4b34e6da8970456c430e55
Wen Kokke <wenkokke@users.noreply.github.com>
2023-08-18 17:15:37 +0100

    Capture stdout

--- vehicle-python/tests/test_pygments.py
+++ vehicle-python/tests/test_pygments.py
@@ -1,7 +1,6 @@
 import difflib
 import os
 import subprocess
-import tempfile
 from pathlib import Path
 
 import pytest
@@ -15,8 +14,7 @@ TESTS_DATA = Path(__file__).parent / "data"
 )  # type: ignore[misc]
 def test_pygments(vehicle_file: Path) -> None:
     UPDATE_GOLDENS = os.environ.get("UPDATE_GOLDENS")
-    temporary_file = tempfile.NamedTemporaryFile("r")
-    subprocess.check_call(
+    actual_tokens = subprocess.check_output(
         [
             "pygmentize",
             "-l",
@@ -24,26 +22,25 @@ def test_pygments(vehicle_file: Path) -> None:
             "-f",
             "tokens",
             str(vehicle_file),
-            "-o",
-            temporary_file.name,
-        ]
+        ],
+        encoding="utf-8",
     )
     vehicle_golden_tokens_file = vehicle_file.with_suffix(".vcl.tokens.golden")
 
     if UPDATE_GOLDENS:
         vehicle_golden_tokens_file.parent.mkdir(parents=True, exist_ok=True)
-        vehicle_golden_tokens_file.write_text(temporary_file.read())
+        vehicle_golden_tokens_file.write_text(actual_tokens)
     elif vehicle_golden_tokens_file.exists():
         with vehicle_golden_tokens_file.open("r") as golden_file:
             diff = list(
                 difflib.unified_diff(
-                    temporary_file.readlines(),
+                    actual_tokens.splitlines(keepends=True),
                     golden_file.readlines(),
-                    fromfile=temporary_file.name,
+                    fromfile=str(vehicle_golden_tokens_file.with_suffix(".actual")),
                     tofile=str(vehicle_golden_tokens_file),
                 )
             )
             if diff:
-                raise ValueError("\n".join(diff))
+                pytest.fail(reason="\n".join(diff))
     else:
         raise FileNotFoundError(str(vehicle_golden_tokens_file))
