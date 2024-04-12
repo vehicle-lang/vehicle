b6d1fe216f6f510295cb9b02b86a559147418657
Wen Kokke <wenkokke@users.noreply.github.com>
2024-04-12 16:42:02 +0100

    Load tests work

--- vehicle-python/tests/test_lossdl2_load.py
+++ vehicle-python/tests/test_lossdl2_load.py
@@ -6,17 +6,14 @@ import pytest
 import vehicle_lang.ast as vcl_ast
 import vehicle_lang.typing as vcl_typing
 
-GOLDEN_PATH = (
-    Path(__file__).parent.parent / "vendor" / "vehicle" / "tests" / "golden" / "compile"
-)
-GOLDEN_LOSS_FUNCTION_FILES = GOLDEN_PATH.glob(os.path.join("**", "DL2Loss.vcl.golden"))
+GOLDEN_PATH = Path(__file__).parent / "data"
+GOLDEN_LOSS_FUNCTION_FILES = GOLDEN_PATH.glob("*.vcl")
 
 
 @pytest.mark.parametrize(
-    "dl2loss_specification_path",
+    "specification_path",
     GOLDEN_LOSS_FUNCTION_FILES,
 )  # type: ignore[misc]
-def test_lossdl2_load(dl2loss_specification_path: Path) -> None:
-    specification_path = dl2loss_specification_path.parent / "spec.vcl"
+def test_lossdl2_load(specification_path: Path) -> None:
     print(f"Load {specification_path}")
     vcl_ast.load(specification_path, target=vcl_typing.DifferentiableLogic.DL2)
