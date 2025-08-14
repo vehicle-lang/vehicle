4c347c0bfddd33f52557dd59cc593764453a5049
Joshua Smart <josh@thesmarts.co.uk>
2025-08-14 13:22:01 +0200

    - Updated dependencies for mathcomp 2.4 - Replaced infix processing with more powerful 'notations' system - Began integration of improved 'mathcomp style' tensor implementation

--- vehicle-rocq/utils.v
+++ vehicle-rocq/utils.v
@@ -11,16 +11,16 @@ Definition existsIndex {n} (f : 'I_n -> Prop) := exists x, f x.
 
 Definition forallIndex {n} (f : 'I_n -> Prop) := forall x, f x.
 
-Section Tensor.
+(* Section Tensor.
 
 Context {R : realType}.
 Context {ds : seq nat}.
 
-Definition eqRatTensorReduced (xs ys : tensor R ds) := reduceAnd (zipWith eq_op xs ys).
+Definition eqRatTensorReduced (xs ys : 'T[R]_(ds, [::])) := reduceAnd (zipWith eq_op xs ys).
 Definition neRatTensorReduced (xs ys : tensor R ds) :=  ~~ (eqRatTensorReduced xs ys).
 Definition leRatTensorReduced (xs ys : tensor R ds) := reduceAnd (zipWith Order.le xs ys).
 Definition ltRatTensorReduced (xs ys : tensor R ds) := reduceAnd (zipWith Order.lt xs ys).
 Definition geRatTensorReduced (xs ys : tensor R ds) := reduceAnd (zipWith Order.ge xs ys).
 Definition gtRatTensorReduced (xs ys : tensor R ds) := reduceAnd (zipWith Order.gt xs ys).
 
-End Tensor.
+End Tensor. *)
