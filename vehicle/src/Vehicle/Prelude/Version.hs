7d5906b6a19da8502f3dd71bf559c0d3d5e6adb9
Matthew Daggitt <matthewdaggitt@gmail.com>
2025-12-03 09:36:47 +0800

    Allows libraries to contain multiple modules (#1011)
    * Allows libraries to contain multiple modules and fixes #999
    
    * Minor fixes
    
    * Two more bugs

--- vehicle/src/Vehicle/Prelude/Version.hs
+++ vehicle/src/Vehicle/Prelude/Version.hs
@@ -9,6 +9,7 @@ module Vehicle.Prelude.Version
     DecodeResult (..),
     encodeAndWriteVersioned,
     readAndDecodeVersioned,
+    isDirtyRepo,
   )
 where
 
@@ -26,6 +27,14 @@ import Paths_vehicle qualified as Cabal (version)
 
 --------------------------------------------------------------------------------
 -- Current versions
+
+#ifdef releaseBuild
+isDirtyRepo :: Bool
+isDirtyRepo = False
+#else
+isDirtyRepo :: Bool
+isDirtyRepo = $(gitDirtyTracked)
+#endif
 
 -- | The imprecise current version of Vehicle. This can be used whenever the
 -- precise version doesn't really matters. This is used when writing out the
@@ -64,7 +73,7 @@ preciseVehicleVersion = showVersion Cabal.version
       hash = $(gitHash)
 
       -- Check if any tracked files have uncommitted changes
-      dirty | $(gitDirtyTracked) = ".dirty"
+      dirty | isDirtyRepo = ".dirty"
             | otherwise          = ""
 
       -- Abbreviate a commit hash while keeping it unambiguous
