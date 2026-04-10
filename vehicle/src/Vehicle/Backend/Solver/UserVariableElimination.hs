a644b90baf0d222ad7adda0f2685876e35a23a78
Gusts Gustavs Grinbergs <ggustavsg@gmail.com>
2026-04-10 14:25:10 +0200

    fix: variable elim IDimNil case

--- vehicle/src/Vehicle/Backend/Solver/UserVariableElimination.hs
+++ vehicle/src/Vehicle/Backend/Solver/UserVariableElimination.hs
@@ -247,6 +247,9 @@ eliminateTensorAssertion ::
   m (Value Builtin)
 eliminateTensorAssertion op (TensorOp2Args dims xs ys) =
   case dims of
+    IDimNil -> do
+      -- For scalar comparisons, directly apply the comparison
+      evalCompareRatTensor op (TensorOp2Args IDimNil xs ys)
     IDimCons d@(INatLiteral n) ds -> do
       -- TODO switch to use `etaReduceTensor`?
       nameCtx <- getNameContext
