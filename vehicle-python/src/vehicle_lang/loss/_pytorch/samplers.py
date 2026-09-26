a71781fcebb07628b6136c1685f1317bec752514
Matthew Daggitt <matthewdaggitt@gmail.com>
2026-09-26 15:07:48 +0800

    Fix wording

--- vehicle-python/src/vehicle_lang/loss/_pytorch/samplers.py
+++ vehicle-python/src/vehicle_lang/loss/_pytorch/samplers.py
@@ -31,10 +31,9 @@ class PyTorchSampler(ABCSampler[Sequence[int], torch.Tensor]):
 
 class DefaultPyTorchSampler(PyTorchSampler):
     """
-    Default sampler implementation for PyTorch that uses FGSM attack.
-
-    Uses Fast Gradient Sign Method (FGSM) to generate adversarial samples,
-    descending the search_lambda so that the samples approximate its infimum.
+    Default sampler implementation for PyTorch that uses Projected Gradient Descent attack
+    to generate adversarial samples, descending the search_lambda so that the samples approximate 
+    its infimum.
     """
 
     def __init__(
